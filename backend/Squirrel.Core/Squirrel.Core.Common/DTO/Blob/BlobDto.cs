2ad651cefc54857cd44690d5bc6e2c3bf3cb9849
Zoranais <unikronusus@gmail.com>
2023-08-21 18:58:16 +0300

    sql-22: + BlobDto

---
+++ backend/Squirrel.Core/Squirrel.Core.Common/DTO/Blob/BlobDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Squirrel.Core.Common.DTO.Blob
+{
+    public class BlobDto
+    {
+        public string Id { get; set; } = string.Empty;
+        public string? AbsoluteUri { get; set; }
+        public string ContentType { get; set; } = "octet-stream";
+        public byte[]? Content { get; set; }
+    }
+}

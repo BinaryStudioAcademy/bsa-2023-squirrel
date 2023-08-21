1c475a53c9b6133a4a9bf523c8f4d3736ff6ec07
VladTkach <vladand2003@gmail.com>
2023-08-21 17:36:52 +0300

    add user register dto

---
+++ frontend/src/app/models/auth/user-register-dto.ts
@@ -0,0 +1,5 @@
+export interface UserRegisterDto {
+    username: string;
+    email: string;
+    password: string;
+}
--- frontend/src/app/modules/landing/registration/registration.component.ts
+++ frontend/src/app/modules/landing/registration/registration.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 
+import { UserRegisterDto } from '../../../models/auth/user-register-dto';
+
 @Component({
     selector: 'app-registration',
     templateUrl: './registration.component.html',
@@ -37,6 +39,14 @@ export class RegistrationComponent implements OnInit {
     }
 
     register() {
-        console.log(this.registerForm.value);
+        const userRegistrationData: UserRegisterDto = {
+            username: this.registerForm.value.username,
+            email: this.registerForm.value.email,
+            password: this.registerForm.value.password,
+        };
+
+        // temporary solution
+        // eslint-disable-next-line no-console
+        console.log(userRegistrationData);
     }
 }

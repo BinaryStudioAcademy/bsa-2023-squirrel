32523c9da1b8c6bb7cf2ffa7a164ba9af890bc2d
VladTkach <vladand2003@gmail.com>
2023-08-21 16:40:33 +0300

    add reactive registration form and validation

--- frontend/src/app/modules/landing/registration/registration.component.ts
+++ frontend/src/app/modules/landing/registration/registration.component.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-registration',
     templateUrl: './registration.component.html',
     styleUrls: ['./registration.component.sass'],
 })
-export class RegistrationComponent {
+export class RegistrationComponent implements OnInit {
+    registerForm: FormGroup = new FormGroup({});
+
+    private fb: FormBuilder;
+
+    constructor(fb: FormBuilder) {
+        this.fb = fb;
+    }
+
+    ngOnInit() {
+        this.initializeForm();
+    }
+
+    initializeForm() {
+        this.registerForm = this.fb.group({
+            username: ['', Validators.required],
+            email: ['', Validators.required],
+            password: ['', Validators.required],
+            confirmPassword: ['', Validators.required],
+        });
+    }
+
+    register() {
+        console.log(this.registerForm.value);
+    }
 }

593b837d31855d0359672a2b37affcbd77786067
Nazar Kotyuha <nazar.kotyuhaa@gmail.com>
2023-08-21 14:48:56 +0300

    squirrel-34: + added IMongoService interface

---
+++ backend/Squirrel.Core/Squirrel.Core.BLL/Interfaces/IMongoService.cs
@@ -0,0 +1,17 @@
+﻿using Squirrel.Core.Common.DTO;
+
+namespace Squirrel.Core.BLL.Interfaces
+{
+    public interface IMongoService
+    {
+        Task<ICollection<SampleDto>> GetAllAsync();
+
+        Task<SampleDto> GetByIdAsync(int sampleId);
+
+        Task<SampleDto> CreateAsync(NewSampleDto sampleDto);
+
+        Task<SampleDto> UpdateAsync(int sampleId, NewSampleDto sampleDto);
+
+        Task DeleteAsync(int sampleId);
+    }
+}

a7e880243f1e187937ab0b0bc83ddfe9a3ff76ba
serhii.yasenev@gmail.com <serhii.yasenev@gmail.com>
2023-08-21 14:23:00 +0300

    + add Angular Material UI

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { SharedModule } from '@shared/shared.module';
 
+import { MaterialModule } from './material/material.module';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, SharedModule, AppRoutingModule],
+    imports: [BrowserModule, SharedModule, AppRoutingModule, MaterialModule],
     providers: [],
     bootstrap: [AppComponent],
 })

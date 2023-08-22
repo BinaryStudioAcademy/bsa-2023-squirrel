e0a53c811fc05d1514f4c436bd0c642fdf68b150
Zoranais <unikronusus@gmail.com>
2023-08-22 11:59:30 +0300

    sql-22: * refactoring

--- backend/Squirrel.AzureBlobStorage.WebApi/Services/AzureBlobStorageService.cs
+++ backend/Squirrel.AzureBlobStorage.WebApi/Services/AzureBlobStorageService.cs
@@ -35,35 +35,36 @@ namespace Squirrel.AzureBlobStorage.WebApi.Services
         {
             var blobClient = await GetBlobClientInternalAsync(containerName, blob.Id);
 
-            if (await blobClient.ExistsAsync())
+            if (!await blobClient.ExistsAsync())
             {
-                var blobHttpHeader = new BlobHttpHeaders { ContentType = blob.ContentType };
-                await blobClient.UploadAsync(new BinaryData(blob.Content ?? new byte[] { }),
-                    new BlobUploadOptions { HttpHeaders = blobHttpHeader });
-
-                return blob;
+                throw new InvalidOperationException($"Blob with id:{blob.Id} does not exist.");
             }
-            throw new InvalidOperationException($"Blob with id:{blob.Id} does not exist.");
+
+            var blobHttpHeader = new BlobHttpHeaders { ContentType = blob.ContentType };
+            await blobClient.UploadAsync(new BinaryData(blob.Content ?? new byte[] { }),
+                new BlobUploadOptions { HttpHeaders = blobHttpHeader });
+
+            return blob;
         }
 
         public async Task<Blob> DownloadAsync(string containerName, string blobId)
         {
             var blobClient = await GetBlobClientInternalAsync(containerName, blobId);
 
-            if (await blobClient.ExistsAsync())
+            if (!await blobClient.ExistsAsync())
             {
-                var content = await blobClient.DownloadContentAsync();
-
-                Blob blob = new Blob
-                {
-                    Id = blobId,
-                    Content = content.Value.Content.ToArray(),
-                    ContentType = content.Value.Details.ContentType,
-                };
-
-                return blob;
+                throw new InvalidOperationException($"Blob with id:{blobId} does not exist.");
             }
-            throw new InvalidOperationException($"Blob with id:{blobId} does not exist.");
+
+            var content = await blobClient.DownloadContentAsync();
+            Blob blob = new Blob
+            {
+                Id = blobId,
+                Content = content.Value.Content.ToArray(),
+                ContentType = content.Value.Details.ContentType,
+            };
+
+            return blob;
         }
 
         public async Task<bool> DeleteAsync(string containerName, string blobId)

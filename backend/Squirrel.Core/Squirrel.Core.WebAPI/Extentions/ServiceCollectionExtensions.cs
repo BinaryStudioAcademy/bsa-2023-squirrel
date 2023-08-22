2462c6c3583b2d30d67879e0d9c1772c90711d2a
Oleh Karapysh <sebastianvell777@gmail.com>
2023-08-22 12:20:15 +0300

    sql-3: + Jwt options registered from appsettings

--- backend/Squirrel.Core/Squirrel.Core.WebAPI/Extentions/ServiceCollectionExtensions.cs
+++ backend/Squirrel.Core/Squirrel.Core.WebAPI/Extentions/ServiceCollectionExtensions.cs
@@ -6,6 +6,10 @@ using Squirrel.Core.WebAPI.Validators;
 using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Squirrel.Core.Common.JWT;
 
 namespace Squirrel.Core.WebAPI.Extentions
 {
@@ -16,7 +20,7 @@ namespace Squirrel.Core.WebAPI.Extentions
             services
                 .AddControllers()
                 .AddNewtonsoftJson(options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
-
+            services.AddScoped<JwtIssuerOptions>();
             services.AddTransient<ISampleService, SampleService>();
         }
 
@@ -39,6 +43,62 @@ namespace Squirrel.Core.WebAPI.Extentions
                 options.UseSqlServer(
                     connectionsString,
                     opt => opt.MigrationsAssembly(typeof(SquirrelCoreContext).Assembly.GetName().Name)));
+        }
+        
+        public static void ConfigureJwtAuth(this IServiceCollection services, IConfiguration configuration)
+        {
+            var jwtAppSettingOptions = configuration.GetSection(nameof(JwtIssuerOptions))!;
+            // Get secret key from appsettings for testing.
+            var secretKey = jwtAppSettingOptions[nameof(JwtIssuerOptions.SecretJwtKey)];
+            var signingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secretKey!));
+            
+            services.Configure<JwtIssuerOptions>(options =>
+            {
+                options.Issuer = jwtAppSettingOptions[nameof(JwtIssuerOptions.Issuer)]!;
+                options.Audience = jwtAppSettingOptions[nameof(JwtIssuerOptions.Audience)]!;
+                options.SigningCredentials = new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256);
+            });
+
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidIssuer = jwtAppSettingOptions[nameof(JwtIssuerOptions.Issuer)],
+
+                ValidateAudience = true,
+                ValidAudience = jwtAppSettingOptions[nameof(JwtIssuerOptions.Audience)],
+
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = signingKey,
+
+                RequireExpirationTime = false,
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero
+            };
+
+            services
+                .AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(configureOptions =>
+                {
+                    configureOptions.ClaimsIssuer = jwtAppSettingOptions[nameof(JwtIssuerOptions.Issuer)];
+                    configureOptions.TokenValidationParameters = tokenValidationParameters;
+                    configureOptions.SaveToken = true;
+                    configureOptions.Events = new JwtBearerEvents
+                    {
+                        OnAuthenticationFailed = context =>
+                        {
+                            if (context.Exception.GetType() == typeof(SecurityTokenExpiredException))
+                            {
+                                context.Response.Headers.Add("Token-Expired", "true");
+                            }
+
+                            return Task.CompletedTask;
+                        }
+                    };
+                });
         }
     }
 }
--- backend/Squirrel.Core/Squirrel.Core.WebAPI/Program.cs
+++ backend/Squirrel.Core/Squirrel.Core.WebAPI/Program.cs
@@ -19,6 +19,7 @@ builder.Services.RegisterCustomServices();
 builder.Services.AddAutoMapper();
 builder.Services.AddSwaggerGen();
 builder.Services.AddValidation();
+builder.Services.ConfigureJwtAuth(builder.Configuration);
 
 builder.Services.AddCors();
 builder.Services.AddHealthChecks();

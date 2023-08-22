618fc3de66f246b47adece14eb8c33c6e5ad865a
Zoranais <unikronusus@gmail.com>
2023-08-22 14:56:23 +0300

    sql-22: - AzureBlobStorage.WebApi

--- backend/Squirrel.AzureBlobStorage.WebApi/Models/Blob.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Squirrel.AzureBlobStorage.WebApi.Models
-{
-    public class Blob
-    {
-        public string Id { get; set; } = string.Empty;
-        public string ContentType { get; set; } = "octet-stream";
-        public byte[]? Content { get; set; }
-    }
-}

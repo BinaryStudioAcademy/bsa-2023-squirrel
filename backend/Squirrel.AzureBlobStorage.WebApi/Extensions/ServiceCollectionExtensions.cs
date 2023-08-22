a97856fc8e0e9618e1c702960baaa83b05a48b0d
Zoranais <unikronusus@gmail.com>
2023-08-22 12:27:06 +0300

    sql-22: + extension methods

---
+++ backend/Squirrel.AzureBlobStorage.WebApi/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Azure;
+using Squirrel.AzureBlobStorage.WebApi.Interfaces;
+using Squirrel.AzureBlobStorage.WebApi.Services;
+
+namespace Squirrel.AzureBlobStorage.WebApi.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void RegisterCustomServices(this IServiceCollection services)
+        {
+            services
+                .AddControllers()
+                .AddNewtonsoftJson(options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
+            services.AddTransient<IBlobStorageService, AzureBlobStorageService>();
+        }
+
+        public static void AddAzureBlobStorage(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAzureClients(clientBuilder =>
+            {
+                clientBuilder.AddBlobServiceClient(configuration.GetConnectionString("BlobStorageConnectionString"), preferMsi: true);
+            });
+        }
+    }
+}

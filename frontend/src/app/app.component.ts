fed780df766d730635942d119a23ce8b90e6bd14
serhii.yasenev@gmail.com <serhii.yasenev@gmail.com>
2023-08-22 16:48:23 +0300

    + add GoogleLoginProvider

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
+import { GoogleLoginProvider, SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
 import { SpinnerService } from '@core/services/spinner.service';
 
 @Component({
     selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.sass'],
     template: '<app-loading-spinner></app-loading-spinner><router-outlet></router-outlet>',
 })
-export class AppComponent {
-    constructor(private router: Router, private spinner: SpinnerService) {
+export class AppComponent implements OnInit {
+    loginForm!: FormGroup;
+
+    socialUser!: SocialUser;
+
+    isLoggedIn?: boolean;
+
+    constructor(
+        private router: Router,
+        private spinner: SpinnerService,
+        private formBuilder: FormBuilder,
+        private socialAuthService: SocialAuthService,
+    ) {
         this.listenRouter();
+    }
+
+    ngOnInit() {
+        this.loginForm = this.formBuilder.group({
+            email: ['', Validators.required],
+            password: ['', Validators.required],
+        });
+        this.socialAuthService.authState.subscribe((user) => {
+            this.socialUser = user;
+            this.isLoggedIn = user != null;
+            console.log(this.socialUser);
+        });
+    }
+
+    loginWithGoogle(): void {
+        this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID);
+    }
+
+    logOut(): void {
+        this.socialAuthService.signOut();
     }
 
     private listenRouter() {
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { GoogleLoginProvider, SocialAuthServiceConfig, SocialLoginModule } from '@abacritt/angularx-social-login';
 import { SharedModule } from '@shared/shared.module';
 
 import { MaterialModule } from './material/material.module';
@@ -8,8 +10,26 @@ import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, SharedModule, AppRoutingModule, MaterialModule],
-    providers: [],
+    imports: [BrowserModule, SharedModule, AppRoutingModule, MaterialModule, ReactiveFormsModule, SocialLoginModule],
+    providers: [
+        {
+            provide: 'SocialAuthServiceConfig',
+            useValue: {
+                autoLogin: false,
+                providers: [
+                    {
+                        id: GoogleLoginProvider.PROVIDER_ID,
+                        provider: new GoogleLoginProvider(
+                            '914510104037-k5ft2q9qjia33jgmh9mo3nm99ea04kbn.apps.googleusercontent.com',
+                        ),
+                    },
+                ],
+                onError: (err) => {
+                    console.error(err);
+                },
+            } as SocialAuthServiceConfig,
+        },
+    ],
     bootstrap: [AppComponent],
 })
 export class AppModule {}

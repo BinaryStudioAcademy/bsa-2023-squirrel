bb5c6df281a7a6eff40bb7175d0dfc1795d113a0
Oleh Karapysh <sebastianvell777@gmail.com>
2023-08-22 12:10:47 +0300

    sql-3: + accessToken class

---
+++ backend/Squirrel.Core/Squirrel.Core.Common/JWT/AccessToken.cs
@@ -0,0 +1,13 @@
+﻿namespace Squirrel.Core.Common.JWT;
+
+public sealed class AccessToken
+{
+    public string Token { get; }
+    public int ExpiresIn { get; }
+
+    public AccessToken(string token, int expiresIn)
+    {
+        Token = token;
+        ExpiresIn = expiresIn;
+    }
+}

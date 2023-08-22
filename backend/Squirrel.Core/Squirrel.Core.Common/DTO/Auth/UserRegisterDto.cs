2a8e89b4a4cd0f35973ac157cc8ee6b780931e84
Oleh Karapysh <sebastianvell777@gmail.com>
2023-08-22 12:08:11 +0300

    sql-3: + DTOs for login and register info

---
+++ backend/Squirrel.Core/Squirrel.Core.Common/DTO/Auth/UserRegisterDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Squirrel.Core.Common.DTO.Auth;
+
+public sealed class UserRegisterDto : UserLoginDto
+{
+    public string Username { get; set; }
+    public string ConfirmPassword { get; set; }
+}

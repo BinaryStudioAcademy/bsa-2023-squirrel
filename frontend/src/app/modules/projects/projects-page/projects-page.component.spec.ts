9e0f7e6707877a9a7f7c9ab0714068ffab71e6b8
Illia Lopukhovych <ilyalopukhovich@gmail.com>
2023-08-22 15:51:45 +0300

    sql-35: + created files and folders for project-page

---
+++ frontend/src/app/modules/projects/projects-page/projects-page.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProjectsPageComponent } from './projects-page.component';
+
+describe('ProjectsPageComponent', () => {
+  let component: ProjectsPageComponent;
+  let fixture: ComponentFixture<ProjectsPageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ProjectsPageComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ProjectsPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

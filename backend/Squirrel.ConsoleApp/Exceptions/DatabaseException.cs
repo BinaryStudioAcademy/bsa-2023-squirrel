9fd1ccb40f9671f88e9dc57cf7ca50280e1cbc5b
Zoranais <unikronusus@gmail.com>
2023-08-22 18:57:03 +0300

    + custom Database exception

---
+++ backend/Squirrel.ConsoleApp/Exceptions/DatabaseException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Squirrel.ConsoleApp.Exceptions
+{
+    public class DatabaseException : Exception
+    {
+        public DatabaseException(string? message) : base(message)
+        {}
+    }
+}

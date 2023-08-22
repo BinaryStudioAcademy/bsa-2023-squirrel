9d795133e2cbcfbd6463f98831d6aafb72bd41fe
Zoranais <unikronusus@gmail.com>
2023-08-22 11:50:55 +0300

    sql-22: + AzureBlobStorageService

---
+++ backend/Squirrel.AzureBlobStorage.WebApi/Services/AzureBlobStorageService.cs
@@ -0,0 +1,94 @@
+﻿using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs;
+using Squirrel.AzureBlobStorage.WebApi.Interfaces;
+using Squirrel.AzureBlobStorage.WebApi.Models;
+using System;
+
+namespace Squirrel.AzureBlobStorage.WebApi.Services
+{
+    public class AzureBlobStorageService: IBlobStorageService
+    {
+        private readonly BlobServiceClient _blobServiceClient;
+
+        public AzureBlobStorageService(BlobServiceClient blobServiceClient)
+        {
+            _blobServiceClient = blobServiceClient;
+        }
+
+        public async Task<Blob> UploadAsync(string containerName, Blob blob)
+        {
+            var blobClient = await GetBlobClientInternalAsync(containerName, blob.Id);
+
+            if (await blobClient.ExistsAsync())
+            {
+                throw new InvalidOperationException($"Blob with id:{blob.Id} already exists.");
+            }
+
+            var blobHttpHeader = new BlobHttpHeaders { ContentType = blob.ContentType };
+            await blobClient.UploadAsync(new BinaryData(blob.Content ?? new byte[] { }),
+                new BlobUploadOptions { HttpHeaders = blobHttpHeader });
+
+            return blob;
+        }
+
+        public async Task<Blob> UpdateAsync(string containerName, Blob blob)
+        {
+            var blobClient = await GetBlobClientInternalAsync(containerName, blob.Id);
+
+            if (await blobClient.ExistsAsync())
+            {
+                var blobHttpHeader = new BlobHttpHeaders { ContentType = blob.ContentType };
+                await blobClient.UploadAsync(new BinaryData(blob.Content ?? new byte[] { }),
+                    new BlobUploadOptions { HttpHeaders = blobHttpHeader });
+
+                return blob;
+            }
+            throw new InvalidOperationException($"Blob with id:{blob.Id} does not exist.");
+        }
+
+        public async Task<Blob> DownloadAsync(string containerName, string blobId)
+        {
+            var blobClient = await GetBlobClientInternalAsync(containerName, blobId);
+
+            if (await blobClient.ExistsAsync())
+            {
+                var content = await blobClient.DownloadContentAsync();
+
+                Blob blob = new Blob
+                {
+                    Id = blobId,
+                    Content = content.Value.Content.ToArray(),
+                    ContentType = content.Value.Details.ContentType,
+                };
+
+                return blob;
+            }
+            throw new InvalidOperationException($"Blob with id:{blobId} does not exist.");
+        }
+
+        public async Task DeleteAsync(string containerName, string blobId)
+        {
+            var blobClient = await GetBlobClientInternalAsync(containerName, blobId);
+
+            if (!await blobClient.ExistsAsync())
+            {
+                throw new InvalidOperationException($"Blob with id:{blobId} does not exist.");
+            }
+            await blobClient.DeleteAsync();
+        }
+
+        private async Task<BlobContainerClient> GetOrCreateContainerByNameAsync(string name)
+        {
+            var containerClient = _blobServiceClient.GetBlobContainerClient(name);
+            await containerClient.CreateIfNotExistsAsync();
+
+            return containerClient;
+        }
+
+        private async Task<BlobClient> GetBlobClientInternalAsync(string containerName, string blobName)
+        {
+            var container = await GetOrCreateContainerByNameAsync(containerName);
+            return container.GetBlobClient(blobName);
+        }
+    }
+}

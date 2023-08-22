39589bb22f88621267dfbfe0056720256e8c9bda
Zoranais <unikronusus@gmail.com>
2023-08-22 11:26:44 +0300

    sql-22: + basic structure of AzureBlobStorage project

---
+++ backend/Squirrel.AzureBlobStorage.WebApi/Controllers/AzureBlobStorageController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Squirrel.AzureBlobStorage.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AzureBlobStorageController : ControllerBase
+    {
+        
+    }
+}
---
+++ backend/Squirrel.AzureBlobStorage.WebApi/Program.cs
@@ -0,0 +1,36 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", reloadOnChange: true, optional: true)
+    .AddEnvironmentVariables()
+    .Build();
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+    endpoints.MapHealthChecks("/health");
+});
+
+app.UseHttpsRedirection();
+
+app.Run();

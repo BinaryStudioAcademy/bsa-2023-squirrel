1d55d5fa17ea1381d26a64990b726b6f6f1138b7
Zoranais <unikronusus@gmail.com>
2023-08-22 11:33:33 +0300

    sql-22: + IBlobStorageService

---
+++ backend/Squirrel.AzureBlobStorage.WebApi/Interfaces/IBlobStorageService.cs
@@ -0,0 +1,12 @@
+﻿using Squirrel.AzureBlobStorage.WebApi.Models;
+
+namespace Squirrel.AzureBlobStorage.WebApi.Interfaces
+{
+    public interface IBlobStorageService
+    {
+        Task<Blob> UploadAsync(string containerName, Blob blob);
+        Task<Blob> UpdateAsync(string containerName, Blob blob);
+        Task<Blob> DownloadAsync(string containerName, string blobId);
+        Task DeleteAsync(string containerName, string blobId);
+    }
+}

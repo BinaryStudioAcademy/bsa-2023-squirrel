0f4618fb59c2cb62a7db97e165e51a7c55a5e5be
Zoranais <unikronusus@gmail.com>
2023-08-23 12:29:45 +0300

    44: * refactoring

--- backend/Squirrel.ConsoleApp/Services/PostgreSqlService.cs
+++ backend/Squirrel.ConsoleApp/Services/PostgreSqlService.cs
@@ -1,81 +1,21 @@
 ﻿using Npgsql;
-using Squirrel.ConsoleApp.Exceptions;
-using Squirrel.ConsoleApp.Interfaces;
-using System.Text;
+using Squirrel.ConsoleApp.Services.Abstract;
 
-namespace Squirrel.ConsoleApp.Services
+namespace Squirrel.ConsoleApp.Services;
+
+public class PostgreSqlService : BaseDbService
 {
-    public class PostgreSqlService : IDatabaseService
+    public PostgreSqlService(string connectionString): base(connectionString) {}
+
+    public override string ExecuteQuery(string query)
     {
-        private string _connectionString;
+        using var connection = new NpgsqlConnection(ConnectionString);
+        return ExecuteQueryFromConnectionInternal(connection, query);
+    }
 
-        public string ConnectionString
-        {
-            get { return _connectionString; }
-            set { _connectionString = value; }
-        }
-
-        public PostgreSqlService(string connectionString)
-        {
-            _connectionString = connectionString;
-        }
-
-        public async Task<string> ExecuteQueryAsync(string query)
-        {
-            using NpgsqlConnection connection = new NpgsqlConnection(_connectionString);
-
-            using NpgsqlCommand command = connection.CreateCommand();
-            command.CommandText = query;
-            try
-            {
-                await connection.OpenAsync();
-                NpgsqlDataReader reader = await command.ExecuteReaderAsync();
-                
-                var result = BuildQueryResultString(reader);
-                await reader.CloseAsync();
-
-                return result;
-            }
-            catch (Exception ex)
-            {
-                throw new DatabaseException(ex.Message);
-            }
-        }
-
-        public string ExecuteQuery(string query)
-        {
-            using NpgsqlConnection connection = new NpgsqlConnection(_connectionString);
-
-            using NpgsqlCommand command = connection.CreateCommand();
-            command.CommandText = query;
-            try
-            {
-                connection.Open();
-                NpgsqlDataReader reader = command.ExecuteReader();
-
-                var result = BuildQueryResultString(reader);
-                reader.Close();
-
-                return result;
-            }
-            catch (Exception ex)
-            {
-                throw new DatabaseException(ex.Message);
-            }
-        }
-
-        private string BuildQueryResultString(NpgsqlDataReader reader)
-        {
-            var stringBuilder = new StringBuilder();
-            while (reader.Read())
-            {
-                for (int i = 0; i < reader.FieldCount; i++)
-                {
-                    stringBuilder.AppendLine(reader[i] + "\t");
-                }
-            }
-
-            return stringBuilder.ToString();
-        }
+    public override async Task<string> ExecuteQueryAsync(string query)
+    {
+        using var connection = new NpgsqlConnection(ConnectionString);
+        return await ExecuteQueryFromConnectionInternalAsync(connection, query);
     }
 }
--- backend/Squirrel.ConsoleApp/Services/SqlServerService.cs
+++ backend/Squirrel.ConsoleApp/Services/SqlServerService.cs
@@ -1,82 +1,21 @@
-﻿using Squirrel.ConsoleApp.Exceptions;
-using Squirrel.ConsoleApp.Interfaces;
+﻿using Squirrel.ConsoleApp.Services.Abstract;
 using System.Data.SqlClient;
-using System.Text;
 
-namespace Squirrel.ConsoleApp.Services
+namespace Squirrel.ConsoleApp.Services;
+
+public class SqlServerService : BaseDbService
 {
-    public class SqlServerService : IDatabaseService
+    public SqlServerService(string connectionString): base(connectionString) {}
+
+    public override string ExecuteQuery(string query)
     {
-        private string _connectionString;
+        using var connection = new SqlConnection(ConnectionString);
+        return ExecuteQueryFromConnectionInternal(connection, query);
+    }
 
-        public string ConnectionString
-        {
-            get { return _connectionString; }
-            set { _connectionString = value; }
-        }
-
-        public SqlServerService(string connectionString)
-        {
-            _connectionString = connectionString;
-        }
-
-        public async Task<string> ExecuteQueryAsync(string query)
-        {
-            using SqlConnection connection = new SqlConnection(_connectionString);
-
-            using SqlCommand command = connection.CreateCommand();
-            command.CommandText = query;
-            try
-            {
-                await connection.OpenAsync();
-                SqlDataReader reader = await command.ExecuteReaderAsync();
-
-                var result = BuildQueryResultString(reader);
-                reader.Close();
-
-                return result;
-            }
-            catch (Exception ex)
-            {
-                throw new DatabaseException(ex.Message);
-            }
-        }
-
-        public string ExecuteQuery(string query)
-        {
-            using SqlConnection connection = new SqlConnection(_connectionString);
-
-            using SqlCommand command = connection.CreateCommand();
-            command.CommandText = query;
-            try
-            {
-                connection.Open();
-                SqlDataReader reader = command.ExecuteReader();
-
-                var result = BuildQueryResultString(reader);
-                reader.Close();
-
-                return result;
-            }
-            catch (Exception ex)
-            {
-                throw new DatabaseException(ex.Message);
-            }
-
-        }
-
-        private string BuildQueryResultString(SqlDataReader reader)
-        {
-            var stringBuilder = new StringBuilder();
-            while (reader.Read())
-            {
-                for (int i = 0; i < reader.FieldCount; i++)
-                {
-                    stringBuilder.AppendLine(reader[i] + "\t");
-                }
-            }
-
-            return stringBuilder.ToString();
-        }
+    public override async Task<string> ExecuteQueryAsync(string query)
+    {
+        using var connection = new SqlConnection(ConnectionString);
+        return await ExecuteQueryFromConnectionInternalAsync(connection, query);
     }
 }

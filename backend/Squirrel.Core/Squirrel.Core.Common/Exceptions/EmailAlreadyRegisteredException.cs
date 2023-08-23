de64246be4d2c374431dbbbcd21927af3c06d73b
Oleh Karapysh <sebastianvell777@gmail.com>
2023-08-23 17:00:25 +0300

    added exceptions for invalid registration info

---
+++ backend/Squirrel.Core/Squirrel.Core.Common/Exceptions/EmailAlreadyRegisteredException.cs
@@ -0,0 +1,8 @@
+﻿namespace Squirrel.Core.Common.Exceptions;
+
+public sealed class EmailAlreadyRegisteredException : Exception
+{
+    public EmailAlreadyRegisteredException() : base("Email is already registered. Try another one")
+    {
+    }
+}
---
+++ backend/Squirrel.Core/Squirrel.Core.Common/Exceptions/UsernameAlreadyRegisteredException.cs
@@ -0,0 +1,8 @@
+﻿namespace Squirrel.Core.Common.Exceptions;
+
+public sealed class UsernameAlreadyRegisteredException : Exception
+{
+    public UsernameAlreadyRegisteredException() : base("Username is already registered. Try another one")
+    {
+    }
+}

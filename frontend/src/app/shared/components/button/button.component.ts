2d5aa5a48049cb9044b72687c1fbd3571201a261
Nikita <deryabin2001nikita@gmail.com>
2023-08-23 21:51:34 +0300

    Added variant property,  that is used for define the class.

--- frontend/src/app/shared/components/button/button.component.ts
+++ frontend/src/app/shared/components/button/button.component.ts
@@ -1,23 +1,49 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.sass']
 })
-export class ButtonComponent {
-
+export class ButtonComponent implements OnChanges{
   @Input() text = '';
 
   @Input() width = 'auto';
   
   @Input() height = 'auto';
 
+  @Input() padding = "10px 20px";
+
   @Input() fontSize = '16px';
 
-  @Input() btnClass = "";
+  @Input() variant = "outline-primary";
+
+  @Input() isDisabled = false;
 
   @Output() buttonOnClick: EventEmitter<void> = new EventEmitter<void>();
+
+  btnClass = "";
+
+  ngOnChanges(): void {
+    switch (this.variant){
+      case "outline-primary":{
+        this.btnClass = "outline-primary-button";
+        break;
+      }
+      case "outline-secondary":{
+        this.btnClass = "outline-secondary-button";
+        break;
+      }
+      case "filled": {
+        this.btnClass = "filled-button";
+        break;
+      }
+    }
+
+    if(this.isDisabled == true){
+      this.btnClass="disabled-button";
+    }
+  }
 
   handleClick(): void{
     this.buttonOnClick.emit();

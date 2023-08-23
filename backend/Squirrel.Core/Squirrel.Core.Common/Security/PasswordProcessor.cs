ae5acea04e328b18064913a5a2b7962d306b2ad7
Oleh Karapysh <sebastianvell777@gmail.com>
2023-08-23 14:57:47 +0300

    added PasswordProcessor for hashing user passwords

---
+++ backend/Squirrel.Core/Squirrel.Core.Common/Security/PasswordProcessor.cs
@@ -0,0 +1,20 @@
+﻿namespace Squirrel.Core.Common.Security;
+
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+
+public static class PasswordProcessor
+{
+    public static string HashPassword(string password, byte[] salt)
+        => Convert.ToBase64String(
+            KeyDerivation.Pbkdf2(
+                password,
+                salt,
+                prf: KeyDerivationPrf.HMACSHA256,
+                iterationCount: 10000,
+                numBytesRequested: 32
+            )
+        );
+    
+    public static bool ValidatePassword(string password, string hash, string salt)
+        => HashPassword(password, Convert.FromBase64String(salt)) == hash;
+}

f2983ba3f4b3548ba33c909021ecce57e31f8ff5
Nazar Kotyuha <nazar.kotyuhaa@gmail.com>
2023-08-24 12:48:42 +0300

    34: + added mongo service for future use

--- backend/Squirrel.Core/Squirrel.Core.WebAPI/Program.cs
+++ backend/Squirrel.Core/Squirrel.Core.WebAPI/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddControllers();
 builder.Services.AddSquirrelCoreContext(builder.Configuration);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.RegisterCustomServices();
+builder.Services.RegisterCustomServices(builder.Configuration);
 builder.Services.AddAutoMapper();
 builder.Services.AddSwaggerGen();
 builder.Services.AddValidation();

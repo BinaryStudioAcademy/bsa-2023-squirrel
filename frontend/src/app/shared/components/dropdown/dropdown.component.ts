97b605a4e9603c49baac9f695c643352cbb6c8bf
Anastasiia Nosulich <anastasiia.nosulich.pz.2019@lpnu.ua>
2023-08-25 00:10:50 +0300

    added collapsing the dropdown by clicking anywhere outside the component

--- frontend/src/app/shared/components/dropdown/dropdown.component.ts
+++ frontend/src/app/shared/components/dropdown/dropdown.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, HostListener, Input, OnInit, Output } from '@angular/core';
 
 @Component({
     selector: 'app-dropdown',
@@ -15,6 +15,16 @@ export class DropdownComponent implements OnInit {
     @Output() selectedValueChanged = new EventEmitter<string>();
 
     selectedOption: string;
+
+    @HostListener('document:click', ['$event'])
+    onClick(event: Event): void {
+        if (!this.elementRef.nativeElement.contains(event.target)) {
+            this.isActive = false;
+        }
+    }
+
+    // eslint-disable-next-line no-empty-function
+    constructor(private elementRef: ElementRef) {}
 
     ngOnInit(): void {
         [this.selectedOption] = this.options;

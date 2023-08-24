b973161a0d5f2a445604c75c0c97b8303a674615
alina sielina <alina.sielina@binary-studio.com>
2023-08-25 02:00:04 +0300

    fix linting errors

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreModule } from '@core/core.module';
 import { SharedModule } from '@shared/shared.module';
 
 import { MaterialModule } from './material/material.module';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { CoreModule } from '@core/core.module';
 
 @NgModule({
     declarations: [AppComponent],
@@ -14,4 +14,4 @@ import { CoreModule } from '@core/core.module';
     providers: [],
     bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }

5cddcc07345e50187ec59afd6773746e71d30c94
VladTkach <vladand2003@gmail.com>
2023-08-26 13:28:54 +0300

    add connection route

---
+++ backend/Squirrel.ConsoleApp/Controllers/SettingController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Squirrel.ConsoleApp.Interfaces;
+using Squirrel.ConsoleApp.Models;
+
+namespace Squirrel.ConsoleApp.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SettingController: ControllerBase
+{
+    private readonly IConnectionFileService _connectionFileService;
+
+    public SettingController(IConnectionFileService connectionFileService)
+    {
+        _connectionFileService = connectionFileService;
+    }
+    
+    [HttpPost]
+    [Route("connect")]
+    public IActionResult Post(ConnectionString connectionString)
+    {
+        Console.WriteLine(connectionString);
+        _connectionFileService.SaveToFile(connectionString);
+
+        var randomId = Guid.NewGuid();
+        return Ok(randomId);
+    }
+}

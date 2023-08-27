63bd33e12b264d89f9dcc42f22fba0a176430b4f
Zoranais <unikronusus@gmail.com>
2023-08-28 01:11:53 +0300

    51: + MatChipsModule to MaterialModule

--- frontend/src/app/material/material.module.ts
+++ frontend/src/app/material/material.module.ts
@@ -1,13 +1,31 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
     declarations: [],
-    imports: [CommonModule, MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule],
-    exports: [CommonModule, MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule],
+    imports: [
+        CommonModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatCardModule,
+        MatAutocompleteModule,
+        MatChipsModule,
+    ],
+    exports: [
+        CommonModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatCardModule,
+        MatAutocompleteModule,
+        MatChipsModule,
+    ],
 })
 export class MaterialModule {}

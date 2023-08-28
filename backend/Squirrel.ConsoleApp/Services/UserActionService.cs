7ed7d96558d3170f18943e5be28dc2467398b284
serhii.yasenev@gmail.com <serhii.yasenev@gmail.com>
2023-08-28 14:08:31 +0300

    update UserActionService

--- backend/Squirrel.ConsoleApp/Services/UserActionService.cs
+++ backend/Squirrel.ConsoleApp/Services/UserActionService.cs
@@ -27,47 +27,23 @@ namespace Squirrel.ConsoleApp.Services
 
         public async Task<UserAction> GetTableDataAsync(string tableName, int rowsCount)
         {
-            var query = DatabaseFactory.GetTableDataQuery(_dbType, tableName, rowsCount);
-            var result = await _databaseService.ExecuteQueryAsync(query);
-
-            var data = string.Join(Environment.NewLine, result.Rows.Select(ConvertRowToString));
-
-            return new UserAction
-            {
-                Name = $"Data from '{tableName}' for first '{rowsCount}' rows",
-                Type = DataType.TableData,
-                Data = data
-            };
+            return await GetUserActionAsync($"Data from '{tableName}' Table for first '{rowsCount}' rows",
+                                            DataType.TableData,
+                                            DatabaseFactory.GetTableDataQuery(_dbType, tableName, rowsCount));
         }
 
         public async Task<UserAction> GetFunctionAsync(string functionName)
         {
-            var query = DatabaseFactory.GetFunctionQuery(_dbType, functionName);
-            var result = await _databaseService.ExecuteQueryAsync(query);
-
-            var data = string.Join(Environment.NewLine, result.Rows.Select(ConvertRowToString));
-
-            return new UserAction
-            {
-                Name = $"Data from '{functionName}' Function",
-                Type = DataType.Function,
-                Data = data
-            };
+            return await GetUserActionAsync($"Data from '{functionName}' Function",
+                                            DataType.Function,
+                                            DatabaseFactory.GetFunctionQuery(_dbType, functionName));
         }
 
         public async Task<UserAction> GetStoredProcedureAsync(string storedProcedureName)
         {
-            var query = DatabaseFactory.GetStoredProcedureQuery(_dbType, storedProcedureName);
-            var result = await _databaseService.ExecuteQueryAsync(query);
-
-            var data = string.Join(Environment.NewLine, result.Rows.Select(ConvertRowToString));
-
-            return new UserAction
-            {
-                Name = $"Data from '{storedProcedureName}' StoredProcedure",
-                Type = DataType.StoredProcedure,
-                Data = data
-            };
+            return await GetUserActionAsync($"Data from '{storedProcedureName}' StoredProcedure",
+                                            DataType.StoredProcedure,
+                                            DatabaseFactory.GetStoredProcedureQuery(_dbType, storedProcedureName));
         }
 
         private async Task<IEnumerable<UserAction>> GetActionsAsync(DataType dataType, string query)
@@ -79,6 +55,17 @@ namespace Squirrel.ConsoleApp.Services
                 Type = dataType,
                 Data = ConvertRowToString(row)
             });
+        }
+
+        private async Task<UserAction> GetUserActionAsync(string name, DataType dataType, string query)
+        {
+            var result = await _databaseService.ExecuteQueryAsync(query);
+            return new UserAction
+            {
+                Name = name,
+                Type = dataType,
+                Data = string.Join(Environment.NewLine, result.Rows.Select(ConvertRowToString))
+            };
         }
 
         private string ConvertRowToString(string[] row) => string.Join(", ", row);

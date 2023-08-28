4e7d544f641b3c1d1e61df335818d7b1e82b7237
Zoranais <unikronusus@gmail.com>
2023-08-28 14:53:37 +0300

    51: * Remove unused test data

--- frontend/src/app/modules/pull-request/pull-request/pull-request.component.ts
+++ frontend/src/app/modules/pull-request/pull-request/pull-request.component.ts
@@ -14,45 +14,8 @@ import { User } from 'src/app/models/user/user';
 export class PullRequestComponent {
     @Input() public pullRequest: PullRequest;
 
-    constructor(private timeService: TimeSpanStringifierService) {
-        const user = {
-            id: 1,
-            avatarUrl: 'https://picsum.photos/200',
-            email: 'test@test.test',
-            firstName: 'John',
-            lastName: 'Smith',
-            userName: 'Johnny',
-        } as User;
-        const branch = {
-            id: 1,
-            name: 'DEV',
-        } as Branch;
-        const date = new Date(1478708162000);
-        const sourceBranch = {
-            id: 1,
-            name: 'bug/1191-status-delete',
-        } as Branch;
-
-        this.pullRequest = {
-            id: 1,
-            author: user,
-            comments: [
-                {
-                    id: 1,
-                    author: user,
-                    content: 'test',
-                    createdAt: date,
-                    updatedAt: date,
-                } as Comment,
-            ],
-            sourceBranch,
-            destinationBranch: branch,
-            title: '#1111 Fix random DROP DATABASE execution',
-            createdAt: date,
-            updatedAt: date,
-            reviewers: [user, user, user, user],
-        } as PullRequest;
-    }
+    // eslint-disable-next-line no-empty-function
+    constructor(private timeService: TimeSpanStringifierService) {}
 
     public calculateTime(date: Date): string {
         return this.timeService.stringify(date);

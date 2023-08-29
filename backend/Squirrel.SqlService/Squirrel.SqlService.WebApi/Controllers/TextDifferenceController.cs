36921b803b43a71680cbd87c707c8c45abbf7d62
Zoranais <unikronusus@gmail.com>
2023-08-29 15:34:20 +0300

    63: + endpoints for TextService

---
+++ backend/Squirrel.SqlService/Squirrel.SqlService.WebApi/Controllers/TextDifferenceController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Squirrel.Core.Common.DTO.Text;
+using Squirrel.SqlService.WebApi.Interfaces;
+
+namespace Squirrel.SqlService.WebApi.Controllers;
+
+[Route("api/text-difference")]
+[ApiController]
+public class TextDifferenceController : ControllerBase
+{
+    private readonly ITextService _textService;
+
+    public TextDifferenceController(ITextService textService)
+    {
+        _textService = textService;
+    }
+
+    /// <summary>
+    /// Finds side-by-side differences in a pair of texts.
+    /// </summary>
+    [HttpPost("side-by-side")]
+    public ActionResult<SideBySideDiffResultDto> FindSideBySideDifferences([FromBody] TextPairRequestDto textPairDto)
+    {
+        return Ok(_textService.GetSideBySideDiffs(textPairDto));
+    }
+
+    /// <summary>
+    /// Finds inline differences in a pair of texts.
+    /// </summary>
+    [HttpPost("inline")]
+    public ActionResult<InLineDiffResultDto> FindInLineDifferences([FromBody] TextPairRequestDto linePairDto)
+    {
+        return Ok(_textService.GetInlineDiffs(linePairDto));
+    }
+}

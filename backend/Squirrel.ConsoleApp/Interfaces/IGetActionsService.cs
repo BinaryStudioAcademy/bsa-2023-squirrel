5ba8cb47f5e7c02021212c4f0499befa6d2f7d92
serhii.yasenev@gmail.com <serhii.yasenev@gmail.com>
2023-08-29 10:50:41 +0300

    upd IGetActionsService

--- backend/Squirrel.ConsoleApp/Interfaces/IGetActionsService.cs
+++ backend/Squirrel.ConsoleApp/Interfaces/IGetActionsService.cs
@@ -4,10 +4,12 @@ namespace Squirrel.ConsoleApp.Interfaces;
 
 public interface IGetActionsService
 {
-    Task<UserAction> GetAllFunctionsAsync();
-    Task<UserAction> GetFunctionAsync(string functionName);
-    Task<UserAction> GetAllStoredProceduresAsync();
-    Task<UserAction> GetStoredProcedureAsync(string storedProcedureName);
     Task<UserAction> GetAllTablesAsync();
     Task<TableData> GetTableDataAsync(string tableName, int rowsCount);
+
+    Task<UserAction> GetAllFunctionsAsync();
+    Task<UserAction> GetFunctionAsync(string functionName);
+
+    Task<UserAction> GetAllStoredProceduresAsync();
+    Task<UserAction> GetStoredProcedureAsync(string storedProcedureName);
 }

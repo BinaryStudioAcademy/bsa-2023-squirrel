b75c92287e4ac2dbf6806394772d138c6773484d
Oleh Karapysh <sebastianvell777@gmail.com>
2023-08-30 19:53:36 +0300

    added UserService

---
+++ backend/Squirrel.Core/Squirrel.Core.BLL/Interfaces/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using Squirrel.Core.Common.DTO.Auth;
+using Squirrel.Core.DAL.Entities;
+
+namespace Squirrel.Core.BLL.Interfaces;
+
+public interface IUserService
+{
+    Task<User?> GetUserByEmailAsync(string email);
+    Task<User?> GetUserByUsernameAsync(string username);
+    Task<User> CreateUserAsync(UserRegisterDto userDto, bool isGoogleAuth);
+}
---
+++ backend/Squirrel.Core/Squirrel.Core.BLL/Services/UserService.cs
@@ -0,0 +1,83 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Squirrel.Core.BLL.Interfaces;
+using Squirrel.Core.BLL.Services.Abstract;
+using Squirrel.Core.Common.DTO.Auth;
+using Squirrel.Core.Common.Security;
+using Squirrel.Core.DAL.Context;
+using Squirrel.Core.DAL.Entities;
+using Squirrel.Shared.Exceptions;
+
+namespace Squirrel.Core.BLL.Services;
+
+public sealed class UserService : BaseService, IUserService
+{
+    private const int MaxNameLength = 25;
+    
+    public UserService(SquirrelCoreContext context, IMapper mapper) : base(context, mapper)
+    {
+    }
+
+    public async Task<User?> GetUserByEmailAsync(string email)
+        => await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+
+    public async Task<User?> GetUserByUsernameAsync(string username)
+        => await _context.Users.FirstOrDefaultAsync(u => u.Username == username);
+
+    public async Task<User> CreateUserAsync(UserRegisterDto userDto, bool isGoogleAuth)
+    {
+        if (await GetUserByUsernameAsync(userDto.Username) is not null)
+        {
+            if (isGoogleAuth)
+            {
+                // Google registration must not fail because of the same username.
+                userDto.Username = GenerateRandomUsername();
+            }
+            else
+            {
+                throw new UsernameAlreadyRegisteredException();
+            }
+        }
+
+        if (await GetUserByEmailAsync(userDto.Email) is not null)
+        {
+            throw new EmailAlreadyRegisteredException();
+        }
+
+        var newUser = PrepareNewUserData(userDto, isGoogleAuth);
+        var createdUser = (await _context.Users.AddAsync(newUser)).Entity;
+        await _context.SaveChangesAsync();
+
+        return createdUser;
+    }
+
+    private string GenerateRandomUsername()
+        => ("user" + SecurityUtils.GenerateRandomSalt()).Substring(MaxNameLength);
+
+    private void TruncateName(UserRegisterDto user)
+    {
+        user.FirstName = user.FirstName.Substring(MaxNameLength);
+        user.LastName = user.LastName.Substring(MaxNameLength);
+    }
+
+    private void HashUserPassword(User newUser, string password)
+    {
+        var salt = SecurityUtils.GenerateRandomSalt();
+        newUser.Salt = salt;
+        newUser.PasswordHash = SecurityUtils.HashPassword(password, salt);
+    }
+
+    private User PrepareNewUserData(UserRegisterDto userDto, bool isGoogleAuth)
+    {
+        // User's name from Google account might be too long, so we need to truncate it.
+        TruncateName(userDto);
+        var newUser = _mapper.Map<User>(userDto)!;
+        newUser.IsGoogleAuth = isGoogleAuth;
+        if (!isGoogleAuth)
+        {
+            HashUserPassword(newUser, userDto.Password);
+        }
+
+        return newUser;
+    }
+}

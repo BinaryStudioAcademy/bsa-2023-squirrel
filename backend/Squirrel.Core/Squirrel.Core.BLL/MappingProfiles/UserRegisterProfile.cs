2243c7dd19745e0105151228fd4aebaa643b6549
Oleh Karapysh <sebastianvell777@gmail.com>
2023-08-30 19:54:18 +0300

    added mapping from GoogleToken to UserRegisterDto

---
+++ backend/Squirrel.Core/Squirrel.Core.BLL/MappingProfiles/UserRegisterProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Google.Apis.Auth;
+using Squirrel.Core.Common.DTO.Auth;
+
+namespace Squirrel.Core.BLL.MappingProfiles;
+
+public sealed class UserRegisterProfile : Profile
+{
+    public UserRegisterProfile()
+    {
+        CreateMap<GoogleJsonWebSignature.Payload, UserRegisterDto>()
+            .ForMember(m => m.Email, s => s.MapFrom(f => f.Email))
+            .ForMember(m => m.FirstName, s => s.MapFrom(f => f.GivenName))
+            .ForMember(m => m.LastName, s => s.MapFrom(f => f.FamilyName))
+            .ForMember(m => m.Username, s => s.MapFrom(f => f.Name));
+    }
+}

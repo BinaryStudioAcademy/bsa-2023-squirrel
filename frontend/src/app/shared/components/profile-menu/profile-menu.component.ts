b71f04461b1211e2dd85f6c9c0af02e3a2b4dd80
Illia Lopukhovych <ilyalopukhovich@gmail.com>
2023-08-31 13:47:43 +0300

    sql-16: * edited pngs

--- frontend/src/app/shared/components/profile-menu/profile-menu.component.ts
+++ frontend/src/app/shared/components/profile-menu/profile-menu.component.ts
@@ -5,7 +5,4 @@ import { Component } from '@angular/core';
     templateUrl: './profile-menu.component.html',
     styleUrls: ['./profile-menu.component.sass'],
 })
-export class ProfileMenuComponent {
-    // eslint-disable-next-line no-undef,no-empty-function,@typescript-eslint/no-useless-constructor,@typescript-eslint/no-empty-function
-    constructor() { }
-}
+export class ProfileMenuComponent { }

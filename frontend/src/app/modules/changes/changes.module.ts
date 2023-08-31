f4c9ff0b1573f5ccff3a323c9b21fc77dba2b9ac
VladTkach <vladand2003@gmail.com>
2023-08-31 17:58:04 +0300

    added btn to changes and edited styles

--- frontend/src/app/modules/changes/changes.module.ts
+++ frontend/src/app/modules/changes/changes.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { ChangesComponent } from './changes/changes.component';
 import { SolutionComponent } from './solution/solution.component';
 import { ChangesRoutingModule } from './changes-routing.module';
+import {SharedModule} from "@shared/shared.module";
 
 @NgModule({
     declarations: [
@@ -13,6 +14,7 @@ import { ChangesRoutingModule } from './changes-routing.module';
     imports: [
         CommonModule,
         ChangesRoutingModule,
+        SharedModule,
     ],
 })
 export class ChangesModule { }

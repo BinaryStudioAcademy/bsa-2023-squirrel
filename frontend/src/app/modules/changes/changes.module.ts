7f46211553e64e1f487662d49026e1f2b4f2d3a1
VladTkach <vladand2003@gmail.com>
2023-08-31 18:03:05 +0300

    added resize icon

--- frontend/src/app/modules/changes/changes.module.ts
+++ frontend/src/app/modules/changes/changes.module.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SharedModule } from '@shared/shared.module';
 
 import { ChangesComponent } from './changes/changes.component';
 import { SolutionComponent } from './solution/solution.component';
 import { ChangesRoutingModule } from './changes-routing.module';
-import {SharedModule} from "@shared/shared.module";
 
 @NgModule({
     declarations: [

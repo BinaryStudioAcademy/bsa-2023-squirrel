17721541545a7bc85df1488f89597ac255a81893
Anastasiia Nosulich <anastasiia.nosulich.pz.2019@lpnu.ua>
2023-08-31 09:30:20 +0300

    added usage of dropdown

--- frontend/src/app/modules/main/main-page/headers/navbar-header/navbar-header.component.ts
+++ frontend/src/app/modules/main/main-page/headers/navbar-header/navbar-header.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { ComponentType } from '@angular/cdk/portal';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { SafeHtml } from '@angular/platform-browser';
+import { NotFoundComponent } from '@shared/components/not-found/not-found.component';
+import { SvgFileContentFetcher } from '@shared/helpers/svgFileContentFetcher';
 
 @Component({
     selector: 'app-navbar-header',
@@ -6,12 +10,32 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./navbar-header.component.sass'],
 })
 export class NavbarHeaderComponent implements OnInit {
+    @ViewChild('myModalContent') modalContent: TemplateRef<any>;
+
+    modalComponent: ComponentType<NotFoundComponent> = NotFoundComponent;
+
     branches: string[];
 
     selectedBranch: string;
 
+    branchesIconPath = 'assets/git-branch.svg';
+
+    branchesIcon: SafeHtml;
+
+    // eslint-disable-next-line no-empty-function
+    constructor(private svgFileContentFetcher: SvgFileContentFetcher) {}
+
     ngOnInit(): void {
+        this.fetchSvgContent();
         this.branches = ['Branch 1', 'Branch 2', 'Branch 3', 'Branch 4'];
+    }
+
+    fetchSvgContent() {
+        this.svgFileContentFetcher.fetchSvgContent(this.branchesIconPath).subscribe((response) => {
+            if (response !== null) {
+                this.branchesIcon = response;
+            }
+        });
     }
 
     onBranchSelected(value: string) {

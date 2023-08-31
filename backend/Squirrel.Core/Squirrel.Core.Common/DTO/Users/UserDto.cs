e3827023824081bb22859c946638ca5de2d2e7a4
Oleh Karapysh <sebastianvell777@gmail.com>
2023-08-31 17:51:43 +0300

    renamed to AvatarUrl

--- backend/Squirrel.Core/Squirrel.Core.Common/DTO/Users/UserDto.cs
+++ backend/Squirrel.Core/Squirrel.Core.Common/DTO/Users/UserDto.cs
@@ -5,5 +5,5 @@ public sealed class UserDto
     public int Id { get; set; }
     public string Email { get; set; }
     public string UserName { get; set; }
-    public string AvatarLink { get; set; }
+    public string? AvatarUrl { get; set; }
 }

16caf4136be5720d727cbe582ea0747ea6abc31f
Andrew Bertash <107005256+ABertashDev@users.noreply.github.com>
2023-09-01 08:39:37 +0300

    Create SignalR hub on Web API side

---
+++ backend/Squirrel.Core/Squirrel.Core.BLL/Hubs/SquirrelHub.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Squirrel.Core.BLL.Interfaces;
+using Squirrel.Core.Common.DTO.SquirrelHub;
+using Squirrel.Core.DAL.Enums;
+
+namespace Squirrel.Core.BLL.Hubs;
+
+public sealed class SquirrelHub : Hub<ISquirrelHub>
+{
+    public override async Task OnConnectedAsync()
+    {
+        var clientId = Guid.NewGuid().ToString();
+        await Clients.Caller.SetClientId(clientId);
+    }
+
+    // TODO: work with data recived from ConsoleApp (save to Db)
+    public async Task ReceiveData(string clientId, string query, DbEngine dbEngine, QueryResultTableDTO queryResultTableDTO)
+    {
+        Console.WriteLine($"------------------------------------------------------------------");
+        Console.WriteLine($"Successfully get data from user '{clientId}'");
+        Console.WriteLine($"    query:     '{query}'");
+        Console.WriteLine($"    db engine: '{dbEngine}'");
+        Console.WriteLine($"    result:");
+        Console.WriteLine(queryResultTableDTO);
+        Console.WriteLine($"------------------------------------------------------------------");
+    }
+}
---
+++ backend/Squirrel.Core/Squirrel.Core.BLL/Interfaces/ISquirrelHub.cs
@@ -0,0 +1,10 @@
+﻿using Squirrel.Core.DAL.Enums;
+
+namespace Squirrel.Core.BLL.Interfaces;
+
+public interface ISquirrelHub
+{
+    Task SetClientId(string guid);
+    Task ExecuteQuery(string clientId, DbEngine dbEngine, string query);
+}
+

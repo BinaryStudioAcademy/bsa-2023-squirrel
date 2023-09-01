9e5399737db6f0a4c7f193b4e4cf4340a920483f
Andrew Bertash <107005256+ABertashDev@users.noreply.github.com>
2023-09-01 08:41:07 +0300

    Attempt ot use IUserIdProvider

---
+++ backend/Squirrel.Core/Squirrel.Core.BLL/Hubs/CustomUserIdProvider.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Squirrel.Core.BLL.Hubs;
+
+public class CustomUserIdProvider : IUserIdProvider
+{
+    public virtual string GetUserId(HubConnectionContext connection)
+    {
+        // TODO: find a way to set UserIdentifier with value from ConsoleApp
+        // or Delete IUserIdProvider implementation if current way is ok
+        return Guid.NewGuid().ToString();
+    }
+}

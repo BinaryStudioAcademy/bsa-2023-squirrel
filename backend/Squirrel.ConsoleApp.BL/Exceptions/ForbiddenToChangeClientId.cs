e703b9139aaa9380edc176aced3993f0bafc9c4c
Andrew Bertash <107005256+ABertashDev@users.noreply.github.com>
2023-09-01 08:49:33 +0300

    Add exception class (for future)

---
+++ backend/Squirrel.ConsoleApp.BL/Exceptions/ForbiddenToChangeClientId.cs
@@ -0,0 +1,7 @@
+﻿namespace Squirrel.ConsoleApp.BL.Exceptions;
+
+public class ForbiddenToChangeClientId : Exception
+{
+    public ForbiddenToChangeClientId(string path) 
+        : base($"Client ID file with path: {path} is already exist! You can't change Client ID!") { }
+}

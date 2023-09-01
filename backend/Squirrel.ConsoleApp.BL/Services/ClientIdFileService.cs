3d64149a9ee772a11dde64715850ce65cde64b1f
Andrew Bertash <107005256+ABertashDev@users.noreply.github.com>
2023-09-01 08:47:36 +0300

    Implement Service for a work with ClientId

---
+++ backend/Squirrel.ConsoleApp.BL/Services/ClientIdFileService.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using Squirrel.ConsoleApp.BL.Exceptions;
+using Squirrel.ConsoleApp.BL.Interfaces;
+using Squirrel.ConsoleApp.Models.Models;
+
+namespace Squirrel.ConsoleApp.BL.Services;
+
+public class ClientIdFileService : IClientIdFileService
+{
+    private static string ClientIdFilePath
+    {
+        get
+        {
+            string userFolder = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            return Path.Combine(userFolder, "squirrel-client-id.json");
+        }
+    }
+
+
+    public string GetClientId()
+    {
+        var filePath = ClientIdFilePath;
+        if (!File.Exists(filePath))
+        {
+            return "";
+        }
+
+        var guid = File.ReadAllText(filePath);
+        return guid;
+    }
+
+    public void SetClientId(string guid)
+    {
+        var filePath = ClientIdFilePath;
+        File.WriteAllText(filePath, guid);
+    }
+}

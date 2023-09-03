3cf08adf3dd975351601ff8f9185b77c1b46b318
Nazar Kotyuha <nazar.kotyuhaa@gmail.com>
2023-09-03 15:51:41 +0300

    76: + added service to backend

---
+++ backend/Squirrel.Core/Squirrel.Core.WebAPI/Controllers/DatabaseItemsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Squirrel.Shared.DTO.DatabaseItem;
+
+namespace Squirrel.Core.WebAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class DatabaseItemsController : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<List<DatabaseItem>>> GetAllItems()
+    {
+        using (var httpClient = new HttpClient())
+        {
+            // Replace with the actual URL of your microservice's DatabaseItemsController.
+            string sqlMicroservice = "https://localhost:7244/databaseitems";
+
+            // Send a GET request to the microservice.
+            HttpResponseMessage response = await httpClient.GetAsync(sqlMicroservice);
+
+            if (response.IsSuccessStatusCode)
+            {
+                // Read and parse the response content as a list of DatabaseItem objects.
+                List<DatabaseItem>? databaseItems = await response.Content.ReadFromJsonAsync<List<DatabaseItem>>();
+
+                // Return the list as an ActionResult.
+                return Ok(databaseItems);
+            }
+            else
+            {
+                // Handle HTTP error responses here.
+                return StatusCode((int)response.StatusCode, "Failed to retrieve data from microservice.");
+            }
+        }
+    }
+}
+
---
+++ backend/Squirrel.SqlService/Squirrel.SqlService.WebApi/Controllers/DatabaseItemsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Squirrel.Shared.DTO.DatabaseItem;
+using Squirrel.SqlService.BLL.Interfaces;
+
+namespace Squirrel.SqlService.WebApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class DatabaseItemsController : ControllerBase
+{
+    private readonly IDbItemsRetrievalService _dbItemsRetrieval;
+
+    public DatabaseItemsController(IDbItemsRetrievalService dbItemsRetrieval)
+    {
+        _dbItemsRetrieval = dbItemsRetrieval;
+    }
+
+    [HttpGet]
+    public ActionResult<List<DatabaseItem>> GetAllItems()
+    {
+        return Ok(_dbItemsRetrieval.GetAllItems());
+    }
+}

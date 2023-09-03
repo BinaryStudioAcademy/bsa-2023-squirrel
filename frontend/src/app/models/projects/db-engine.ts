061d13b05939153f72acec2821b34bbf39517b67
Oleh Karapysh <sebastianvell777@gmail.com>
2023-09-03 16:19:22 +0300

    fixed code style

--- frontend/src/app/models/projects/db-engine.ts
+++ frontend/src/app/models/projects/db-engine.ts
@@ -1,4 +1,4 @@
 export enum DbEngine {
     SqlServer = 'SqlServer',
-    PostgreSQL = 'PostgreSQL'
+    PostgreSql = 'PostgreSql',
 }
--- frontend/src/app/modules/projects/project-card/project-card.component.ts
+++ frontend/src/app/modules/projects/project-card/project-card.component.ts
@@ -1,16 +1,15 @@
 import { Component, Input } from '@angular/core';
 
-import { ProjectDto } from '../../../models/projects/project-dto';
+import { ProjectDto } from 'src/app/models/projects/project-dto';
 
 @Component({
     selector: 'app-project-card',
     templateUrl: './project-card.component.html',
     styleUrls: ['./project-card.component.sass'],
 })
-
 export class ProjectCardComponent {
-    @Input() project: ProjectDto;
+    @Input() public project: ProjectDto;
 
     // eslint-disable-next-line @typescript-eslint/no-useless-constructor,no-empty-function,@typescript-eslint/no-empty-function
-    constructor() { }
+    constructor() {}
 }

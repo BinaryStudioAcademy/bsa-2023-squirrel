6cee80e5ff2a893dd070d6c2a1627ea4abec74c6
Nazar Kotyuha <nazar.kotyuhaa@gmail.com>
2023-09-03 18:17:50 +0300

    76: + added service to frontend

---
+++ frontend/src/app/core/services/database-items.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { DatabaseItem } from 'src/app/models/database-items/database-item';
+
+import { HttpInternalService } from './http-internal.service';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class DatabaseItemsService {
+    // eslint-disable-next-line no-empty-function
+    constructor(private httpService: HttpInternalService) {}
+
+    getAllItems(): Observable<DatabaseItem[]> {
+        return this.httpService.getRequest<DatabaseItem[]>('/api/databaseitems');
+    }
+}

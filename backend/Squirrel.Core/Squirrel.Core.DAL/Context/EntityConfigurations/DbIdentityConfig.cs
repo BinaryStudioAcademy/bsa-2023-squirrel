6cf419953c027ce8dd3d6ff18f01f76998ec58a3
Oleh Karapysh <sebastianvell777@gmail.com>
2023-09-04 21:30:31 +0300

    added DbIdentity with config

---
+++ backend/Squirrel.Core/Squirrel.Core.DAL/Context/EntityConfigurations/DbIdentityConfig.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Squirrel.Core.DAL.Entities;
+
+namespace Squirrel.Core.DAL.Context.EntityConfigurations;
+
+public sealed class DbIdentityConfig : IEntityTypeConfiguration<DbIdentity>
+{
+    public void Configure(EntityTypeBuilder<DbIdentity> builder)
+    {
+        builder.Property(x => x.DbName).IsRequired().HasMaxLength(100); // Clarify max length.
+        builder.Property(x => x.Guid).IsRequired();
+        builder.Property(x => x.DbEngine).IsRequired();
+
+        builder.HasAlternateKey(x => x.Guid);
+
+        builder.HasOne(x => x.Project)
+               .WithMany(x => x.DbIdentities)
+               .HasForeignKey(x => x.ProjectId)
+               .IsRequired()
+               .OnDelete(DeleteBehavior.NoAction);
+    }
+}
---
+++ backend/Squirrel.Core/Squirrel.Core.DAL/Entities/DbIdentity.cs
@@ -0,0 +1,14 @@
+﻿using Squirrel.Core.DAL.Entities.Common;
+using Squirrel.Core.DAL.Enums;
+
+namespace Squirrel.Core.DAL.Entities;
+
+public sealed class DbIdentity : Entity<int>
+{
+    public string DbName { get; set; } = string.Empty;
+    public DbEngine DbEngine { get; set; }
+    public Guid Guid { get; set; }
+
+    public int ProjectId { get; set; }
+    public Project Project { get; set; } = null!;
+}

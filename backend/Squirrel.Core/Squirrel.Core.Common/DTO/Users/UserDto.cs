fbeef6b0aaa9386ccd2b58f3ad297112ba7d1300
Zoranais <unikronusus@gmail.com>
2023-09-04 11:32:09 +0300

    81: * add required field to UserDto

--- backend/Squirrel.Core/Squirrel.Core.Common/DTO/Users/UserDto.cs
+++ backend/Squirrel.Core/Squirrel.Core.Common/DTO/Users/UserDto.cs
@@ -4,6 +4,8 @@ public sealed class UserDto
 {
     public int Id { get; set; }
     public string Email { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
     public string UserName { get; set; }
     public string? AvatarUrl { get; set; }
 }

c4dc4a87ae5e1d58e0beb2d27b4db55d6730c3b4
VladTkach <vladand2003@gmail.com>
2023-09-04 15:57:34 +0300

    Added id property to project dto

--- backend/Squirrel.Core/Squirrel.Core.Common/DTO/Project/ProjectDto.cs
+++ backend/Squirrel.Core/Squirrel.Core.Common/DTO/Project/ProjectDto.cs
@@ -4,6 +4,7 @@ namespace Squirrel.Core.Common.DTO.Project
 {
     public sealed class ProjectDto
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public DbEngine DbEngine { get; set; }
     }

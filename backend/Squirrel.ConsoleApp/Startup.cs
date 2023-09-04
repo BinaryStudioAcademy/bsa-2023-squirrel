69deb9070dfc4eb4e7f5ec32b3a8c3c50c704314
Andrew Bertash <107005256+ABertashDev@users.noreply.github.com>
2023-09-05 02:23:35 +0300

    + Add DbSettings to Configuration

--- backend/Squirrel.ConsoleApp/Startup.cs
+++ backend/Squirrel.ConsoleApp/Startup.cs
@@ -8,14 +8,16 @@ using Squirrel.ConsoleApp.Filters;
 using Squirrel.ConsoleApp.Models;
 using Squirrel.ConsoleApp.Providers;
 using Squirrel.ConsoleApp.Services;
+using System.Text.Json.Serialization;
 
 namespace Squirrel.ConsoleApp;
 
 public class Startup
 {
     public static IConfiguration Configuration { get; } = new ConfigurationBuilder()
-        .SetBasePath(Directory.GetCurrentDirectory())
+        .SetBasePath(AppContext.BaseDirectory)
         .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddJsonFile(HelperService.GetDbSettingsFilePath(), optional: true, reloadOnChange: true)
         .Build();
 
     public void ConfigureServices(IServiceCollection services)
@@ -35,7 +37,13 @@ public class Startup
                 services.AddSingleton<IDatabaseService, PostgreSqlService>();
                 break;
             default:
-                throw new NotImplementedException($"Database type {databaseType} is not supported.");
+                // When the app is first launched, we create an empty DbSettings file
+                // We don't have a DbType value, but the User can change the DbSettings file using endpoints
+                // This is why we have to ensure that the application works even if we have the wrong DbType
+                // This type of error is handled in provider services
+                services.AddSingleton<IDbQueryProvider, SqlServerQueryProvider>();
+                services.AddSingleton<IDatabaseService, SqlServerService>();
+                break;
         }
 
         services.AddScoped<IConnectionFileService, ConnectionFileService>();
@@ -46,6 +54,8 @@ public class Startup
         {
             options.Filters.Add(typeof(CustomExceptionFilter));
         });
+        services.AddControllers().AddJsonOptions(options =>
+            options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
     }
     
     public void Configure(IApplicationBuilder app)

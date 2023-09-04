e430e8221b8ed364b3a7b72120b682d4169cc257
Oleh Karapysh <sebastianvell777@gmail.com>
2023-09-04 10:30:46 +0300

    added branch service and DTO

---
+++ backend/Squirrel.Core/Squirrel.Core.Common/DTO/Branch/BranchDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Squirrel.Core.Common.DTO.Branch;
+
+public sealed class BranchDto
+{
+    public string Name { get; set; } = null!;
+    public bool IsActive { get; set; }
+    public int ProjectId { get; set; }
+}

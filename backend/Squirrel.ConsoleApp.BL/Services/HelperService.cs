6859fcf61e0bb35d2f89185e742c23abbafc6a88
Andrew Bertash <107005256+ABertashDev@users.noreply.github.com>
2023-09-05 02:22:21 +0300

    + Helper service for getting local files path

---
+++ backend/Squirrel.ConsoleApp.BL/Services/HelperService.cs
@@ -0,0 +1,9 @@
+﻿namespace Squirrel.ConsoleApp.BL.Services;
+
+public static class HelperService
+{
+    public static string GetDbSettingsFilePath()
+    {
+        return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "squirrel-db-settings.json");
+    }
+}

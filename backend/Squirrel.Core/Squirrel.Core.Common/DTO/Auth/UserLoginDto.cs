b604e7e0b3c6626f49be50def06d252254f926a9
Nikita <deryabin2001nikita@gmail.com>
2023-09-04 15:14:57 +0300

    Fixed warnings in Common/DTO.

--- backend/Squirrel.Core/Squirrel.Core.Common/DTO/Auth/UserLoginDto.cs
+++ backend/Squirrel.Core/Squirrel.Core.Common/DTO/Auth/UserLoginDto.cs
@@ -2,6 +2,6 @@
 
 public class UserLoginDto
 {
-    public string Email { get; set; }
-    public string Password { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = null!;
 }
--- backend/Squirrel.Core/Squirrel.Core.Common/DTO/Users/UserDto.cs
+++ backend/Squirrel.Core/Squirrel.Core.Common/DTO/Users/UserDto.cs
@@ -3,7 +3,7 @@
 public sealed class UserDto
 {
     public int Id { get; set; }
-    public string Email { get; set; }
-    public string UserName { get; set; }
+    public string Email { get; set; } = String.Empty;
+    public string UserName { get; set; } = String.Empty;
     public string? AvatarUrl { get; set; }
 }

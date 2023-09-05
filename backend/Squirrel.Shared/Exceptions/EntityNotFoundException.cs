fd32809c76dc4c6d30531dc91d1abe5537510999
VladTkach <vladand2003@gmail.com>
2023-09-05 19:27:10 +0300

    edited not found exception

--- backend/Squirrel.Shared/Exceptions/EntityNotFoundException.cs
+++ backend/Squirrel.Shared/Exceptions/EntityNotFoundException.cs
@@ -6,6 +6,6 @@ namespace Squirrel.Shared.Exceptions;
 
 public sealed class EntityNotFoundException : RequestException
 {
-    public EntityNotFoundException(string name) : base($"Entity {name} was not found.",
+    public EntityNotFoundException(string name) : base($"Entity {name} with such id was not found.",
         ErrorType.NotFound, HttpStatusCode.NotFound) { }
 }

567f723cecfa79d1be4013b369819f156edd10b5
Oleh Karapysh <sebastianvell777@gmail.com>
2023-09-06 21:34:47 +0300

    added basic styling

--- frontend/src/app/modules/scripts/script/script.component.ts
+++ frontend/src/app/modules/scripts/script/script.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-script',
-  templateUrl: './script.component.html',
-  styleUrls: ['./script.component.sass']
+    selector: 'app-script',
+    templateUrl: './script.component.html',
+    styleUrls: ['./script.component.sass'],
 })
-export class ScriptComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ScriptComponent {
+    // eslint-disable-next-line @typescript-eslint/no-useless-constructor, no-empty-function
+    constructor() {}
 }

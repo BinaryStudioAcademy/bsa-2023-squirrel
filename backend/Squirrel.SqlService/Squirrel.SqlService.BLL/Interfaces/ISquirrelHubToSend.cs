f0e2c13c6a6e00a2f72f363ed410f5a9cbceda32
Andrew Bertash <107005256+ABertashDev@users.noreply.github.com>
2023-09-06 23:48:18 +0300

    + Describe send actions and received data interfaces

---
+++ backend/Squirrel.SqlService/Squirrel.SqlService.BLL/Interfaces/ISquirrelHubToSend.cs
@@ -0,0 +1,33 @@
+﻿namespace Squirrel.Core.BLL.Interfaces;
+
+public interface ISquirrelHubToSend
+{
+    Task SetClientId(string guid);
+    
+
+    // Actions
+    Task TestExecuteQueryAsync(string clientId, string filterName, int filterRowsCount);
+
+    Task GetAllTablesNamesAsync(string clientId);
+    Task GetTableDataAsync(string clientId, string tableName, int rowsCount);
+
+    Task GetAllStoredProceduresNamesAsync(string clientId);
+    Task GetStoredProcedureDefinitionAsync(string clientId, string storedProcedureName);
+
+    Task GetAllFunctionsNamesAsync(string clientId);
+    Task GetFunctionDefinitionAsync(string clientId, string functionName);
+
+    Task GetAllViewsNamesAsync(string clientId);
+    Task GetViewDefinitionAsync(string clientId, string viewName);
+
+    Task GetDbTablesStructureAsync(string clientId);
+    Task GetDbTablesCheckAndUniqueConstraintsAsync(string clientId);
+
+    Task GetStoredProceduresWithDetailAsync(string clientId);
+    Task GetFunctionsWithDetailAsync(string clientId);
+    Task GetViewsWithDetailAsync(string clientId);
+
+    Task GetUserDefinedTypesWithDefaultsAndRulesAndDefinitionAsync(string clientId);
+    Task GetUserDefinedTableTypesAsync(string clientId);
+}
+

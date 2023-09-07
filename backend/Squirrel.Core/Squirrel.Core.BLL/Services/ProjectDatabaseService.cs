0b6f397451a99537b722246d9c1db4ed4e86e8a9
Oleh Karapysh <sebastianvell777@gmail.com>
2023-09-07 19:10:32 +0300

    added routes to get and add new ProjectDb

---
+++ backend/Squirrel.Core/Squirrel.Core.BLL/Services/ProjectDatabaseService.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Squirrel.Core.BLL.Interfaces;
+using Squirrel.Core.BLL.Services.Abstract;
+using Squirrel.Core.Common.DTO.ProjectDatabase;
+using Squirrel.Core.DAL.Context;
+using Squirrel.Core.DAL.Entities;
+using Squirrel.Shared.Exceptions;
+
+namespace Squirrel.Core.BLL.Services;
+
+public sealed class ProjectDatabaseService : BaseService, IProjectDatabaseService
+{
+    public ProjectDatabaseService(SquirrelCoreContext context, IMapper mapper) : base(context, mapper)
+    {
+    }
+
+    public async Task<List<string>> GetAllProjectDbNamesAsync()
+        => await _context.ProjectDatabases.Select(x => x.DbName).ToListAsync();
+
+    public async Task<string> AddNewProjectDatabaseAsync(ProjectDatabaseDto dto)
+    {
+        var projectDb = _mapper.Map<ProjectDatabase>(dto);
+
+        if (await _context.Projects.FindAsync(dto.ProjectId) is null)
+        {
+            throw new InvalidProjectException();
+        }
+        
+        var addedProjectDb = (await _context.ProjectDatabases.AddAsync(projectDb)).Entity;
+        await _context.SaveChangesAsync();
+
+        return addedProjectDb.DbName;
+    }
+}
---
+++ backend/Squirrel.Core/Squirrel.Core.WebAPI/Controllers/ProjectDatabaseController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Squirrel.Core.BLL.Interfaces;
+using Squirrel.Core.Common.DTO.ProjectDatabase;
+
+namespace Squirrel.Core.WebAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Authorize]
+public sealed class ProjectDatabaseController : ControllerBase
+{
+    private readonly IProjectDatabaseService _projectDatabaseService;
+
+    public ProjectDatabaseController(IProjectDatabaseService projectDatabaseService)
+    {
+        _projectDatabaseService = projectDatabaseService;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<string>> AddNewProjectDatabase([FromBody] ProjectDatabaseDto databaseDto)
+    {
+        return Ok(await _projectDatabaseService.AddNewProjectDatabaseAsync(databaseDto));
+    }
+
+    [HttpGet("all")]
+    public async Task<ActionResult<List<string>>> GetAllProjectDatabases()
+    {
+        return Ok(await _projectDatabaseService.GetAllProjectDbNamesAsync());
+    }
+}

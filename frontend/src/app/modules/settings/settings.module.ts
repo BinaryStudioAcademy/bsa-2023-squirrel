92b2adfe5d81d2d2147d941c7e605b6ac8470943
Illia Lopukhovych <ilyalopukhovich@gmail.com>
2023-09-08 01:44:24 +0300

    resolved eslint issues

--- frontend/src/app/modules/settings/settings.module.ts
+++ frontend/src/app/modules/settings/settings.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { AddUserModalComponent } from '@modules/settings/add-user-modal/add-user-modal.component';
 import { GeneralSettingsComponent } from '@modules/settings/general-settings/general-settings.component';
 import { SettingsMenuComponent } from '@modules/settings/settings-menu/settings-menu.component';
 import { SettingsPageComponent } from '@modules/settings/settings-page/settings-page.component';
@@ -7,7 +8,6 @@ import { SettingsRoutingModule } from '@modules/settings/settings-routing.model'
 import { TeamSettingsComponent } from '@modules/settings/team-settings/team-settings.component';
 import { UserCardComponent } from '@modules/settings/user-card/user-card.component';
 import { SharedModule } from '@shared/shared.module';
-import {AddUserModalComponent} from "@modules/settings/add-user-modal/add-user-modal.component";
 
 @NgModule({
     declarations: [

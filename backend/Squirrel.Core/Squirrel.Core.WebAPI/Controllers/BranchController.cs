1db8d6901f034e7870d67b50c8321c0e49dd2f93
Zoranais <unikronusus@gmail.com>
2023-09-07 22:37:15 +0300

    98: + add BranchController

---
+++ backend/Squirrel.Core/Squirrel.Core.WebAPI/Controllers/BranchController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Squirrel.Core.BLL.Interfaces;
+using Squirrel.Core.Common.DTO.Branch;
+
+namespace Squirrel.Core.WebAPI.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class BranchController : ControllerBase
+{
+    private readonly IBranchService _branchService;
+
+    public BranchController(IBranchService branchService)
+    {
+        _branchService = branchService;
+    }
+
+    [HttpGet("{projectId}")]
+    public ActionResult<List<BranchDto>> GetAllBranches(int projectId)
+    {
+        return Ok(_branchService.GetAllBranches(projectId));
+    }
+
+    [HttpPost("{projectId}")]
+    public async Task<ActionResult<BranchDto>> AddBranch(int projectId, [FromBody] BranchCreateDto dto) 
+    { 
+        return Ok(await _branchService.AddBranchAsync(projectId, dto));
+    } 
+
+
+}

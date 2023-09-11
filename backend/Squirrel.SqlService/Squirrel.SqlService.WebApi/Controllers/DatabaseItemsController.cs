d33eff9973e6e710107fd746156024ecce8e312a
Maksym Shelestiuk <maxshelestiuk@gmail.com>
2023-09-11 11:13:23 +0300

    * Formatting using new dll with problem

--- backend/Squirrel.SqlService/Squirrel.SqlService.WebApi/Controllers/DatabaseItemsController.cs
+++ backend/Squirrel.SqlService/Squirrel.SqlService.WebApi/Controllers/DatabaseItemsController.cs
@@ -9,15 +9,29 @@ namespace Squirrel.SqlService.WebApi.Controllers;
 public sealed class DatabaseItemsController : ControllerBase
 {
     private readonly IDbItemsRetrievalService _dbItemsRetrieval;
+    private readonly ISqlFormatterService _sqlFormatter;
 
-    public DatabaseItemsController(IDbItemsRetrievalService dbItemsRetrieval)
+    public DatabaseItemsController(IDbItemsRetrievalService dbItemsRetrieval, ISqlFormatterService sqlFormatter)
     {
         _dbItemsRetrieval = dbItemsRetrieval;
+        _sqlFormatter = sqlFormatter;
     }
 
     [HttpGet]
     public ActionResult<List<DatabaseItem>> GetAllItems()
     {
         return Ok(_dbItemsRetrieval.GetAllItems());
+    }
+
+    [HttpGet("formatsql")]
+    public ActionResult<string> GetFormattedSql()
+    {
+        string sql = @"SELECT e.last_name AS name, e.commission_pct comm, e.salary * 12 ""Annual Salary""
+                                FROM scott.employees AS e
+                                WHERE e.salary > 1000 or 1=1
+                                ORDER BY
+                                e.first_name,
+                                e.last_name;";
+        return Ok(sql);
     }
 }

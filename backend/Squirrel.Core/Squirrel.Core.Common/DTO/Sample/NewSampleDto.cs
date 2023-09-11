6672a09ba9953a72700b73250e25da9cce0cccc8
Oleh Karapysh <sebastianvell777@gmail.com>
2023-09-11 13:36:28 +0300

    removed sample endpoint and DTOs

--- backend/Squirrel.Core/Squirrel.Core.Common/DTO/Sample/NewSampleDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Squirrel.Core.Common.DTO.Sample;
-
-public class NewSampleDto
-{
-    public string Title { get; set; } = string.Empty;
-    public string? Body { get; set; }
-    public int CreatedBy { get; set; }
-}

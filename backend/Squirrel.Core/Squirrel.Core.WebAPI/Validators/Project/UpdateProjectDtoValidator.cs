8b254b1761ed717274a990c204aa0663d4b86c55
Zoranais <unikronusus@gmail.com>
2023-09-12 17:09:28 +0300

    118: + add backend validation

---
+++ backend/Squirrel.Core/Squirrel.Core.WebAPI/Validators/Project/UpdateProjectDtoValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using Squirrel.Core.Common.DTO.Project;
+
+namespace Squirrel.Core.WebAPI.Validators.Project;
+
+public class UpdateProjectDtoValidator: AbstractValidator<UpdateProjectDto>
+{
+    public UpdateProjectDtoValidator()
+    {
+        RuleFor(x => x.Name)!.MaximumLength(1000);
+        RuleFor(x => x.Name)!
+            .MinimumLength(3)!
+            .MaximumLength(50);
+    }
+}

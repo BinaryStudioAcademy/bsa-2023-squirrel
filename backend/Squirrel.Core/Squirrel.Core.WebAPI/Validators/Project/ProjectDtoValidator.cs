b67da60c5cc1415c0de870cb501c2b497563c719
Zoranais <unikronusus@gmail.com>
2023-09-13 13:00:14 +0300

    118: * add backend Cyrillic validation

--- backend/Squirrel.Core/Squirrel.Core.WebAPI/Validators/Project/ProjectDtoValidator.cs
+++ backend/Squirrel.Core/Squirrel.Core.WebAPI/Validators/Project/ProjectDtoValidator.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation;
 using Squirrel.Core.Common.DTO.Project;
+using System.Text.RegularExpressions;
 
 namespace Squirrel.Core.WebAPI.Validators.Project;
 
@@ -11,6 +12,7 @@ public class ProjectDtoValidator : AbstractValidator<ProjectDto>
         RuleFor(x => x.DbEngine)!.NotNull();
         RuleFor(x => x.Name)!
             .MinimumLength(3)!
-            .MaximumLength(50);
+            .MaximumLength(50)
+            .Must(x => Regex.IsMatch(x, @"^(?![\u0410-\u044F\u0400-\u04FF]).*$"));
     }
 }
--- backend/Squirrel.Core/Squirrel.Core.WebAPI/Validators/Project/UpdateProjectDtoValidator.cs
+++ backend/Squirrel.Core/Squirrel.Core.WebAPI/Validators/Project/UpdateProjectDtoValidator.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation;
 using Squirrel.Core.Common.DTO.Project;
+using System.Text.RegularExpressions;
 
 namespace Squirrel.Core.WebAPI.Validators.Project;
 
@@ -7,9 +8,10 @@ public class UpdateProjectDtoValidator: AbstractValidator<UpdateProjectDto>
 {
     public UpdateProjectDtoValidator()
     {
-        RuleFor(x => x.Name)!.MaximumLength(1000);
+        RuleFor(x => x.Description)!.MaximumLength(1000);
         RuleFor(x => x.Name)!
             .MinimumLength(3)!
-            .MaximumLength(50);
+            .MaximumLength(50)
+            .Must(x => Regex.IsMatch(x, @"^(?![\u0410-\u044F\u0400-\u04FF]).*$"));
     }
 }

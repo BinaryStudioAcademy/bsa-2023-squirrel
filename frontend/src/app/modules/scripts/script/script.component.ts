a96a5bf9319bac905f23808e994e47f848bfcbf2
Oleh Karapysh <sebastianvell777@gmail.com>
2023-09-14 18:59:48 +0300

    use local image

--- frontend/src/app/modules/scripts/script/script.component.ts
+++ frontend/src/app/modules/scripts/script/script.component.ts
@@ -10,7 +10,7 @@ import { ScriptDto } from 'src/app/models/scripts/script-dto';
 export class ScriptComponent {
     @Input() public script: ScriptDto;
 
-    @Input() public avatarUrl: string = 'https://cdn-icons-png.flaticon.com/512/6596/6596121.png';
+    @Input() public avatarUrl: string = 'assets/profile_icon.svg';
 
     // eslint-disable-next-line @typescript-eslint/no-useless-constructor, no-empty-function, @typescript-eslint/no-empty-function
     constructor() {}

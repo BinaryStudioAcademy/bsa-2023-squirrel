35edbbbcfd43b9ca03e6e85cfaddeb9ee37152b8
Illia Lopukhovych <ilyalopukhovich@gmail.com>
2023-09-15 21:40:48 +0300

    changed author.Id to createdBy field

--- backend/Squirrel.Core/Squirrel.Core.BLL/Services/ProjectService.cs
+++ backend/Squirrel.Core/Squirrel.Core.BLL/Services/ProjectService.cs
@@ -80,7 +80,6 @@ public sealed class ProjectService : BaseService, IProjectService
         var project = await _context.Projects
             .Include(project => project.Tags)
             .Include(project => project.Users)
-            .Include(project => project.Author)
             .FirstOrDefaultAsync(project => project.Id == projectId);
         var currentUserId = _userIdGetter.GetCurrentUserId();
 
@@ -88,7 +87,7 @@ public sealed class ProjectService : BaseService, IProjectService
 
         var mappedProject = _mapper.Map<ProjectResponseDto>(project);
         
-        mappedProject.IsAuthor = project!.Author.Id == currentUserId;
+        mappedProject.IsAuthor = project!.CreatedBy == currentUserId;
 
         return mappedProject;
     }

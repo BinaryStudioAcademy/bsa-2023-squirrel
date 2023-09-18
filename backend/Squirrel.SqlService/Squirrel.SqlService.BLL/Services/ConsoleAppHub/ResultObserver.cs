bb2a3eb0f59ad77c420e735a1abc8509b42d1219
VladTkach <vladand2003@gmail.com>
2023-09-18 15:33:54 +0300

    added timeout

--- backend/Squirrel.SqlService/Squirrel.SqlService.BLL/Services/ConsoleAppHub/ResultObserver.cs
+++ backend/Squirrel.SqlService/Squirrel.SqlService.BLL/Services/ConsoleAppHub/ResultObserver.cs
@@ -1,24 +1,49 @@
-﻿using Squirrel.SqlService.BLL.Models.ConsoleAppHub;
+﻿using System.Collections.Concurrent;
+using Squirrel.Shared.Exceptions;
+using Squirrel.SqlService.BLL.Models.ConsoleAppHub;
 
 namespace Squirrel.SqlService.BLL.Services.ConsoleAppHub;
 
 public class ResultObserver
 {
-    private readonly Dictionary<Guid, TaskCompletionSource<QueryResultTableDTO>> _pendingRequests = new();
+    private readonly ConcurrentDictionary<Guid, TaskCompletionSource<QueryResultTableDTO>> _pendingRequests = new();
+    private const int SecondsToTimeout = 20;
 
     public TaskCompletionSource<QueryResultTableDTO> Register(Guid queryId)
     {
         var tcs = new TaskCompletionSource<QueryResultTableDTO>();
-        _pendingRequests[queryId] = tcs;
+        if (!_pendingRequests.TryAdd(queryId, tcs))
+        {
+            throw new QueryAlreadyExistException(queryId);
+        }
+
+        _ = RemoveIfNotSet(queryId);
+
         return tcs;
     }
 
     public void SetResult(Guid queryId, QueryResultTableDTO queryResultTableDto)
     {
-        if (_pendingRequests.TryGetValue(queryId, out var tcs))
+        if (_pendingRequests.TryRemove(queryId, out var tcs))
         {
-            tcs.SetResult(queryResultTableDto);
-            _pendingRequests.Remove(queryId);
+            tcs.TrySetResult(queryResultTableDto);
+        }
+        else
+        {
+            throw new QueryExpiredException(queryId);
+        }
+    }
+
+    private async Task RemoveIfNotSet(Guid queryId)
+    {
+        await Task.Delay(TimeSpan.FromSeconds(SecondsToTimeout));
+        if (_pendingRequests.TryRemove(queryId, out var removedTcs))
+        {
+            removedTcs.TrySetCanceled();
+        }
+        else
+        {
+            throw new QueryExpiredException(queryId);
         }
     }
 }

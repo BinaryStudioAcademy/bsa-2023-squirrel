dd66bb0c5fad9e89d8675d3f4eaf579fe57bc9ca
Zoranais <unikronusus@gmail.com>
2023-09-20 22:16:48 +0300

    129: * update SelectedItemsDto

--- backend/Squirrel.Shared/DTO/SelectedItems/SelectedItemsDto.cs
+++ backend/Squirrel.Shared/DTO/SelectedItems/SelectedItemsDto.cs
@@ -3,5 +3,9 @@ public class SelectedItemsDto
 {
     public string Guid { get; set; } = null!;
     public int CommitId { get; set; }
-    public List<TreeNodeDto> SelectedItems { get; set; }
+    public ICollection<string> StoredProcedures { get; set; } = new List<string>();
+    public ICollection<string> Tables { get; set; } = new List<string>();
+    public ICollection<string> Types { get; set; } = new List<string>();
+    public ICollection<string> Functions { get; set; } = new List<string>();
+    public ICollection<string> Constraints { get; set; } = new List<string>();
 }

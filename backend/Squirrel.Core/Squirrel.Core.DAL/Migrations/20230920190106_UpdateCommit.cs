894a3b27b0f20d0b6a9808c78178e4992aaed930
Zoranais <unikronusus@gmail.com>
2023-09-20 22:19:48 +0300

    129: * Commit entity

---
+++ backend/Squirrel.Core/Squirrel.Core.DAL/Migrations/20230920190106_UpdateCommit.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Squirrel.Core.DAL.Migrations
+{
+    public partial class UpdateCommit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSaved",
+                table: "Commits",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PostScript",
+                table: "Commits",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PreScript",
+                table: "Commits",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsSaved",
+                table: "Commits");
+
+            migrationBuilder.DropColumn(
+                name: "PostScript",
+                table: "Commits");
+
+            migrationBuilder.DropColumn(
+                name: "PreScript",
+                table: "Commits");
+        }
+    }
+}

5077aee50394a1031294d20bf1bb0e7f2250c16b
Zoranais <unikronusus@gmail.com>
2023-09-20 16:35:56 +0300

    + SelectedItemsDto

---
+++ backend/Squirrel.Shared/DTO/SelectedItems/SelectedItemsDto.cs
@@ -0,0 +1,6 @@
+﻿namespace Squirrel.Shared.DTO.SelectedItems;
+public class SelectedItemsDto
+{
+    public string Guid { get; set; } = null!;
+    public List<TreeNodeDto> SelectedItems { get; set; }
+}

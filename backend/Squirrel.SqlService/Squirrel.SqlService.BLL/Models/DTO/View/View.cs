bb975bc2723f83f7a8aa50891c9ed4917f77d22a
Nazar Kotyuha <nazar.kotyuhaa@gmail.com>
2023-09-20 18:41:24 +0300

    131: * fixed view dto

--- backend/Squirrel.SqlService/Squirrel.SqlService.BLL/Models/DTO/View/View.cs
+++ backend/Squirrel.SqlService/Squirrel.SqlService.BLL/Models/DTO/View/View.cs
@@ -1,7 +1,5 @@
 namespace Squirrel.SqlService.BLL.Models.DTO.View;
 
-public class View
+public class View : BaseDbItem
 {
-    public string Schema { get; set; } = null!;
-    public string Name { get; set; } = null!;
 }
--- backend/Squirrel.SqlService/Squirrel.SqlService.BLL/Models/DTO/View/ViewDetailInfo.cs
+++ backend/Squirrel.SqlService/Squirrel.SqlService.BLL/Models/DTO/View/ViewDetailInfo.cs
@@ -1,8 +1,6 @@
 namespace Squirrel.SqlService.BLL.Models.DTO.View;
 
-public class ViewDetailInfo
+public class ViewDetailInfo : BaseDbItem
 {
-    public string Schema { get; set; } = null!;
-    public string Name { get; set; } = null!;
     public string Definition { get; set; } = null!;
 }

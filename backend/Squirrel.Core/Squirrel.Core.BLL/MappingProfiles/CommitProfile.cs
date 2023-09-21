546da6f4d04f1e9a1a03c71b26f3689f73b818ea
Zoranais <unikronusus@gmail.com>
2023-09-21 14:49:03 +0300

    129: * update mapping profile

--- backend/Squirrel.Core/Squirrel.Core.BLL/MappingProfiles/CommitProfile.cs
+++ backend/Squirrel.Core/Squirrel.Core.BLL/MappingProfiles/CommitProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Squirrel.Core.Common.DTO.Commit;
 using Squirrel.Core.DAL.Entities;
+using Squirrel.Shared.DTO.CommitFile;
 
 namespace Squirrel.Core.BLL.MappingProfiles;
 public class CommitProfile: Profile
@@ -8,5 +9,6 @@ public class CommitProfile: Profile
     public CommitProfile()
     {
         CreateMap<Commit, CommitDto>();
+        CreateMap<CommitFileDto, CommitFile>().ReverseMap();
     }
 }

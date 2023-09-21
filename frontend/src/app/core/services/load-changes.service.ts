6a7e38c0566fca8fc52675864ada00693294fefc
Nazar Kotyuha <nazar.kotyuhaa@gmail.com>
2023-09-21 12:20:35 +0300

    131: * enhanced flow on FE

--- frontend/src/app/core/services/load-changes.service.ts
+++ frontend/src/app/core/services/load-changes.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
-import { finalize, Subject, takeUntil } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 
-import { DatabaseItemsService } from './database-items.service';
 import { HttpInternalService } from './http-internal.service';
-import { SpinnerService } from './spinner.service';
 
 @Injectable({
     providedIn: 'root',
@@ -13,46 +11,11 @@ export class LoadChangesService {
 
     private readonly loadChangesRoutePrefix = '/api/changerecords';
 
-    constructor(
-        private httpClient: HttpInternalService,
-        private databaseItemsService: DatabaseItemsService,
-        private spinner: SpinnerService,
-    ) {
+    constructor(private httpClient: HttpInternalService) {
         // eslint-disable-next-line no-empty-function
     }
 
-    public loadChangesRequest(guid: string) {
-        this.spinner.show();
-
-        this.httpClient.postRequest<string>(`${this.loadChangesRoutePrefix}/${guid}`, null!)
-            .pipe(
-                takeUntil(this.unsubscribe$),
-                finalize(() => this.spinner.hide()),
-            )
-            .subscribe({
-                next: (event) => {
-                    // eslint-disable-next-line no-console
-                    console.log(event);
-                },
-                error: (error) => {
-                    // eslint-disable-next-line no-console
-                    console.log(error);
-                },
-            });
-
-        this.databaseItemsService.getAllItems(guid)
-            .pipe(
-                takeUntil(this.unsubscribe$),
-            )
-            .subscribe({
-                next: (event) => {
-                    // eslint-disable-next-line no-console
-                    console.log(event);
-                },
-                error: (error) => {
-                    // eslint-disable-next-line no-console
-                    console.log(error);
-                },
-            });
+    public loadChangesRequest(guid: string): Observable<string> {
+        return this.httpClient.postRequest<string>(`${this.loadChangesRoutePrefix}/${guid}`, null!);
     }
 }

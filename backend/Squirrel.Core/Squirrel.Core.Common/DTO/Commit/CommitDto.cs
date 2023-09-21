f905dd3e1a82e1f2dbe7af61f0cd38a2f4d0eb2d
Zoranais <unikronusus@gmail.com>
2023-09-21 14:51:12 +0300

    129: * add CommitFileDto list to CommitDto

--- backend/Squirrel.Core/Squirrel.Core.Common/DTO/Commit/CommitDto.cs
+++ backend/Squirrel.Core/Squirrel.Core.Common/DTO/Commit/CommitDto.cs
@@ -1,5 +1,6 @@
 ﻿using Squirrel.Core.Common.DTO.Branch;
 using Squirrel.Core.Common.DTO.Users;
+using Squirrel.Shared.DTO.CommitFile;
 
 namespace Squirrel.Core.Common.DTO.Commit;
 public class CommitDto
@@ -8,6 +9,7 @@ public class CommitDto
     public string Message { get; set; } = string.Empty;
     public UserDto Author { get; set; } = null!;
     public ICollection<BranchDto> Branches = new List<BranchDto>();
+    public ICollection<CommitFileDto> CommitFiles { get; set; } = new List<CommitFileDto>();
     public string PreScript { get; set; } = null!;
     public string PostScript { get; set; } = null!;
 }

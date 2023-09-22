0ba0186cd324afaa46b1a556890bc9446de3e1c4
Oleh Karapysh <sebastianvell777@gmail.com>
2023-09-22 17:58:16 +0300

    added ScriptResultComponent

--- frontend/src/app/modules/scripts/scripts.module.ts
+++ frontend/src/app/modules/scripts/scripts.module.ts
@@ -8,9 +8,10 @@ import { ScriptComponent } from './script/script.component';
 import { ScriptErrorResultComponent } from './script-error-result/script-error-result.component';
 import { ScriptsPageComponent } from './scripts-page/scripts-page.component';
 import { ScriptsRoutingModule } from './scripts-routing.module';
+import { ScriptResultComponent } from './script-result/script-result.component';
 
 @NgModule({
-    declarations: [ScriptComponent, ScriptsPageComponent, CreateScriptModalComponent, ScriptErrorResultComponent],
+    declarations: [ScriptComponent, ScriptsPageComponent, CreateScriptModalComponent, ScriptErrorResultComponent, ScriptResultComponent],
     imports: [CommonModule, ScriptsRoutingModule, SharedModule, CdkListboxModule],
 })
 export class ScriptsModule {}

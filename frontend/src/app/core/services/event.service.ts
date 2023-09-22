d0f61ab8783982e2e650641e25e4688d545e547f
Zoranais <unikronusus@gmail.com>
2023-09-22 05:02:24 +0300

    129: * EventService

--- frontend/src/app/core/services/event.service.ts
+++ frontend/src/app/core/services/event.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 
+import { DatabaseItem } from 'src/app/models/database-items/database-item';
 import { UserDto } from 'src/app/models/user/user-dto';
 
 @Injectable({
@@ -13,5 +14,21 @@ export class EventService {
 
     public userChanged(user: UserDto | undefined) {
         this.onUserChanged.next(user);
+    }
+
+    private onChangesLoaded = new Subject<DatabaseItem[] | undefined>();
+
+    public changesLoadedEvent$ = this.onChangesLoaded.asObservable();
+
+    public changesLoaded(item: DatabaseItem[] | undefined) {
+        this.onChangesLoaded.next(item);
+    }
+
+    private onChangesSaved = new Subject<string | undefined>();
+
+    public changesSavedEvent$ = this.onChangesSaved.asObservable();
+
+    public changesSaved(guid: string | undefined) {
+        this.onChangesSaved.next(guid);
     }
 }

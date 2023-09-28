1ce96537cf7151db085082461534e51fa254ec56
Zoranais <unikronusus@gmail.com>
2023-09-29 01:38:58 +0300

    161: + MergeBranchDto

---
+++ backend/Squirrel.Core/Squirrel.Core.Common/DTO/Branch/MergeBranchDto.cs
@@ -0,0 +1,6 @@
+﻿namespace Squirrel.Core.Common.DTO.Branch;
+public class MergeBranchDto
+{
+    public int SourceId { get; set; }
+    public int DestinationId { get; set; }
+}
---
+++ backend/Squirrel.Core/Squirrel.Core.WebAPI/Validators/Branch/MergeBranchDtoValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Squirrel.Core.Common.DTO.Branch;
+
+namespace Squirrel.Core.WebAPI.Validators.Branch;
+
+public class MergeBranchDtoValidator: AbstractValidator<MergeBranchDto>
+{
+    public MergeBranchDtoValidator()
+    {
+        RuleFor(x => x.SourceId)
+            .NotEqual(x => x.DestinationId);
+
+        RuleFor(x => x.DestinationId)
+            .NotEqual(x => x.SourceId);
+    }
+}

41ab5b76f10046cf1f41018b803bb4e11e9144bf
Nikita <deryabin2001nikita@gmail.com>
2023-09-29 23:21:33 +0300

    changed ContentDifferenceController

--- backend/Squirrel.SqlService/Squirrel.SqlService.WebApi/Controllers/ContentDifferenceController.cs
+++ backend/Squirrel.SqlService/Squirrel.SqlService.WebApi/Controllers/ContentDifferenceController.cs
@@ -20,10 +20,4 @@ public sealed class ContentDifferenceController : ControllerBase
     {
         return Ok(await _contentDifferenceService.GetContentDiffsAsync(commitId, tempBlobId));
     }
-    
-    [HttpGet("getBlobContent/{blobId}")]
-    public async Task<ActionResult<string>> GetBlob(Guid blobId)
-    {
-        return Ok(await _contentDifferenceService.GetContentAsync(blobId));
-    }
 }
